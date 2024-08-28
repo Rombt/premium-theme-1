e2b2b3364d4fe40be7dbf79b2cebd1c257f18152
Rombt <rombtnet@gmail.com>
2024-08-28 15:51:16 +0300

    .

--- app/src/template-parts/sections/header_1/header_1.php
+++ app/src/template-parts/sections/header_1/header_1.php
@@ -1,6 +1,6 @@
 <div class="wrapper-section header-1-wrapper-section">
 	<div class="rmbt-full-width rmbt-header-1-full-width">
-		<div class="rmbt-we-do-block-full-width__bg">
+		<div class="header-1-full-width__bg">
 			<div class="wrap-img">
 				<!-- <img src="<?php echo get_template_directory_uri() ?>/assets/img/.jpg" alt="bg_header-1"> -->
 			</div>

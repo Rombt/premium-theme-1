2e9cc59685a1e527244c14e41c3c86705f651e81
Rombt <rombtnet@gmail.com>
2024-09-09 20:26:52 +0300

    header_wireframes-1

--- app/src/front-page.php
+++ app/src/front-page.php
@@ -4,8 +4,8 @@
 
 <main>
 
-	<?php
-	get_template_part( 'template-parts/sections/header_wireframes/header_wireframes' );
+   <?php
+	get_template_part( 'template-parts/sections/header_wireframes-1/header_wireframes-1' );
 	// get_template_part( 'template-parts/sections/header/header' );
 	// get_template_part( 'template-parts/sections/hero_block/hero_block' );
 	?>

7b047f107f14c4295d2d0211574f0b8165452361
Rombt <rombtnet@gmail.com>
2024-09-16 13:55:05 +0300

    .

--- app/src/template-parts/_templates/components/rmbt-vertical-menu-0/scripts.js
+++ app/src/template-parts/_templates/components/rmbt-vertical-menu-0/scripts.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // const arr_ClassesContVerticalMenu = [
+  //   'rmbt-cont-vertical-menu-0',
+  //   'rmbt-cont-vertical-menu-1',
+  // ];
+
+  const classesContVerticalMenu = 'rmbt-cont-vertical-menu-0';
   const classVerticalMenuOpen = 'vertical-menu-open';
   const classMenuItemDropIcon = 'drop-icon';
   const classMenuItemDropIconOpen = 'drop-icon-open';
@@ -9,180 +15,181 @@ document.addEventListener('DOMContentLoaded', function () {
   const classMenuItemOverflow = 'menu-item-other';
   const textMenuItemOverflow = 'other';
 
-  const contVerticalMenu = document.querySelector('.rmbt-cont-vertical-menu-0');
-  if (!contVerticalMenu) return;
+  const nl_contVerticalMenus = document.querySelectorAll(`.${classesContVerticalMenu}`);
+  if (!nl_contVerticalMenus.length === 0) return;
 
-  const nl_menuItems = contVerticalMenu.querySelectorAll('nav > ul > li');
-  const rectContVerticalMenu = contVerticalMenu.getBoundingClientRect();
-  const bottomContVerticalMenu = rectContVerticalMenu.bottom;
-  const heightContVerticalMenu = rectContVerticalMenu.height;
+  nl_contVerticalMenus.forEach(contVerticalMenu => {
+    const nl_menuItems = contVerticalMenu.querySelectorAll('nav > ul > li');
+    const rectContVerticalMenu = contVerticalMenu.getBoundingClientRect();
+    const bottomContVerticalMenu = rectContVerticalMenu.bottom;
+    const heightContVerticalMenu = rectContVerticalMenu.height;
 
-  let remainingHeight = 0;
-  let numberLastItem = 0;
-  let menuItemOverflow;
-  let menuOverflow;
-  let iconMenuOverflow;
+    let remainingHeight = 0;
+    let numberLastItem = 0;
+    let menuItemOverflow;
+    let menuOverflow;
+    let iconMenuOverflow;
 
-  // contVerticalMenu.style.height =
-  //   contVerticalMenu.parentNode.getBoundingClientRect().height;
-
-  nl_menuItems.forEach((menuItem, indexMenuItem) => {
-    let bottomMenuItem = menuItem.getBoundingClientRect().bottom;
-    menuItem.style.position = 'relative';
-    let nl_menuItemUls = menuItem.querySelectorAll('ul');
-    if (nl_menuItemUls.length > 0) {
-      nl_menuItemUls.forEach(menuItemUl => {
-        let iconDrop = document.createElement('div');
-        let closestLi = menuItemUl.closest('li');
-        closestLi.style.position = 'relative';
-        iconDrop.classList.add(classMenuItemDropIcon);
-        iconDrop.addEventListener('click', e => {
-          let subMenu = closestLi.querySelector('ul');
-          if (
-            !e.target.classList.contains(classMenuItemDropIconOpen) &&
-            !subMenu.classList.contains(classSubMenuIOpen)
-          ) {
-            let nl_otherSubMenusOpened = closestLi.parentNode.querySelectorAll(
-              `li .${classSubMenuIOpen}`
-            );
-            if (nl_otherSubMenusOpened.length > 0) {
-              [...nl_otherSubMenusOpened].reverse().forEach(closeSubMenu);
+    nl_menuItems.forEach((menuItem, indexMenuItem) => {
+      let bottomMenuItem = menuItem.getBoundingClientRect().bottom;
+      menuItem.style.position = 'relative';
+      let nl_menuItemUls = menuItem.querySelectorAll('ul');
+      if (nl_menuItemUls.length > 0) {
+        nl_menuItemUls.forEach(menuItemUl => {
+          let iconDrop = document.createElement('div');
+          let closestLi = menuItemUl.closest('li');
+          closestLi.style.position = 'relative';
+          iconDrop.classList.add(classMenuItemDropIcon);
+          iconDrop.addEventListener('click', e => {
+            let subMenu = closestLi.querySelector('ul');
+            if (
+              !e.target.classList.contains(classMenuItemDropIconOpen) &&
+              !subMenu.classList.contains(classSubMenuIOpen)
+            ) {
+              let nl_otherSubMenusOpened = closestLi.parentNode.querySelectorAll(
+                `li .${classSubMenuIOpen}`
+              );
+              if (nl_otherSubMenusOpened.length > 0) {
+                [...nl_otherSubMenusOpened].reverse().forEach(closeSubMenu);
+              }
+              subMenu.classList.add(classSubMenuIOpen);
+              iconDrop.classList.add(classMenuItemDropIconOpen);
+            } else {
+              closeSubMenu(subMenu);
             }
-            subMenu.classList.add(classSubMenuIOpen);
-            iconDrop.classList.add(classMenuItemDropIconOpen);
-          } else {
-            closeSubMenu(subMenu);
-          }
+          });
+          closestLi.closest('li').append(iconDrop);
         });
-        closestLi.closest('li').append(iconDrop);
-      });
-    }
-    if (bottomMenuItem > bottomContVerticalMenu) {
-      if (!menuOverflow) {
-        menuOverflow = document.createElement('div');
-        menuOverflow.classList.add(classMenuOverflow);
-        menuOverflow.style.display = 'none';
-        contVerticalMenu.prepend(menuOverflow);
       }
-      if (remainingHeight === 0) {
-        remainingHeight =
-          (bottomContVerticalMenu -
-            nl_menuItems[indexMenuItem - 1].getBoundingClientRect().bottom) /
-          indexMenuItem;
-        for (let i = 0; i < nl_menuItems.length; i++) {
-          if (i < indexMenuItem) {
-            nl_menuItems[i].style.top = remainingHeight * (i + 1) + 'px';
-          } else {
-            numberLastItem = i - 1;
-            break;
+      if (bottomMenuItem > bottomContVerticalMenu) {
+        if (!menuOverflow) {
+          menuOverflow = document.createElement('div');
+          menuOverflow.classList.add(classMenuOverflow);
+          menuOverflow.style.display = 'none';
+          contVerticalMenu.prepend(menuOverflow);
+        }
+        if (remainingHeight === 0) {
+          remainingHeight =
+            (bottomContVerticalMenu -
+              nl_menuItems[indexMenuItem - 1].getBoundingClientRect().bottom) /
+            indexMenuItem;
+          for (let i = 0; i < nl_menuItems.length; i++) {
+            if (i < indexMenuItem) {
+              nl_menuItems[i].style.top = remainingHeight * (i + 1) + 'px';
+            } else {
+              numberLastItem = i - 1;
+              break;
+            }
           }
         }
+        menuOverflow.append(menuItem);
       }
-      menuOverflow.append(menuItem);
-    }
-  });
+    });
 
-  if (nl_menuItems[numberLastItem - 1]) {
-    menuItemOverflow = document.createElement('div');
-    menuItemOverflow.classList.add(classMenuItemOverflow);
-    menuItemOverflow.innerText = textMenuItemOverflow;
-    menuItemOverflow.classList.add();
+    if (nl_menuItems[numberLastItem - 1]) {
+      menuItemOverflow = document.createElement('div');
+      menuItemOverflow.classList.add(classMenuItemOverflow);
+      menuItemOverflow.innerText = textMenuItemOverflow;
+      menuItemOverflow.classList.add();
 
-    iconMenuOverflow = document.createElement('div');
-    iconMenuOverflow.classList.add(classIconMenuOverflow);
-    menuItemOverflow.append(iconMenuOverflow);
+      iconMenuOverflow = document.createElement('div');
+      iconMenuOverflow.classList.add(classIconMenuOverflow);
+      menuItemOverflow.append(iconMenuOverflow);
 
-    const heightMenuItemOverflow =
-      contVerticalMenu.getBoundingClientRect().bottom -
-      nl_menuItems[numberLastItem - 1]?.getBoundingClientRect().bottom;
+      const heightMenuItemOverflow =
+        contVerticalMenu.getBoundingClientRect().bottom -
+        nl_menuItems[numberLastItem - 1]?.getBoundingClientRect().bottom;
 
-    menuItemOverflow.style.position = 'absolute';
-    menuItemOverflow.style.height = heightMenuItemOverflow + 'px';
-    menuItemOverflow.style.lineHeight = heightMenuItemOverflow + 'px';
-    menuItemOverflow.style.bottom = '0px';
-    menuItemOverflow.style.left = '0px';
-    menuItemOverflow.style.right = '0px';
-
-    menuOverflow.prepend(nl_menuItems[numberLastItem]);
-
-    contVerticalMenu.append(menuItemOverflow);
-  }
-
-  const nl_overflowItems = contVerticalMenu.querySelectorAll(
-    `.${classMenuOverflow} > li`
-  );
-  const overflowItemsLength = nl_overflowItems.length;
-
-  document.addEventListener('click', e => {
-    if (e.target === menuItemOverflow) {
-      const contVerticalMenuUl = contVerticalMenu.querySelector('nav > ul');
-      let heightContVerticalMenuOpen;
-
-      if (!contVerticalMenu.classList.contains(classVerticalMenuOpen)) {
-        contVerticalMenu.classList.add(classVerticalMenuOpen);
-        contVerticalMenuUl.style.position = 'relative';
-        nl_overflowItems.forEach((overflowItem, i) => {
-          overflowItem.style.top = remainingHeight * (numberLastItem + i + 1) + 'px';
-          contVerticalMenuUl.append(overflowItem);
-
-          if (i === overflowItemsLength - 1) {
-            heightContVerticalMenuOpen =
-              overflowItem.getBoundingClientRect().bottom -
-              contVerticalMenu.getBoundingClientRect().top;
-          }
-        });
-
-        contVerticalMenu.style.height = heightContVerticalMenuOpen + 'px';
-        menuItemOverflow.style.bottom = 'auto';
-        menuItemOverflow.style.top = heightContVerticalMenuOpen + 'px';
-      } else {
-        closeVerticalMenu();
-      }
-    } else if (
-      e.target !== contVerticalMenu &&
-      !e.target.classList.contains(classMenuItemDropIcon) &&
-      !e.target.classList.contains(classMenuItemDropIconOpen)
-    ) {
-      closeVerticalMenu();
-      closeAllSubMenus();
-    }
-  });
-
-  function closeVerticalMenu() {
-    if (!contVerticalMenu.classList.contains(classVerticalMenuOpen)) {
-      return;
-    }
-    contVerticalMenu.classList.remove(classVerticalMenuOpen);
-
-    for (let i = overflowItemsLength; i > 0; i--) {
-      menuOverflow.append(nl_menuItems[nl_menuItems.length - i]);
-    }
-
-    contVerticalMenu.style.height = heightContVerticalMenu + 'px';
-
-    if (menuItemOverflow) {
-      menuItemOverflow.style.top = 'auto';
+      menuItemOverflow.style.position = 'absolute';
+      menuItemOverflow.style.height = heightMenuItemOverflow + 'px';
+      menuItemOverflow.style.lineHeight = heightMenuItemOverflow + 'px';
       menuItemOverflow.style.bottom = '0px';
+      menuItemOverflow.style.left = '0px';
+      menuItemOverflow.style.right = '0px';
+
+      menuOverflow.prepend(nl_menuItems[numberLastItem]);
+
+      contVerticalMenu.append(menuItemOverflow);
     }
-  }
 
-  function closeSubMenu(menu) {
-    let dropIconOpen = menu.closest('li').querySelector(`.${classMenuItemDropIconOpen}`);
+    const nl_overflowItems = contVerticalMenu.querySelectorAll(
+      `.${classMenuOverflow} > li`
+    );
+    const overflowItemsLength = nl_overflowItems.length;
 
-    dropIconOpen.classList.remove(classMenuItemDropIconOpen);
-    menu.classList.remove(classSubMenuIOpen);
-  }
+    document.addEventListener('click', e => {
+      if (e.target === menuItemOverflow) {
+        const contVerticalMenuUl = contVerticalMenu.querySelector('nav > ul');
+        let heightContVerticalMenuOpen;
 
-  function closeAllSubMenus() {
-    [...contVerticalMenu.querySelectorAll(`.${classSubMenuIOpen}`)]
-      .reverse()
-      .forEach(closeSubMenu);
-  }
+        if (!contVerticalMenu.classList.contains(classVerticalMenuOpen)) {
+          contVerticalMenu.classList.add(classVerticalMenuOpen);
+          contVerticalMenuUl.style.position = 'relative';
+          nl_overflowItems.forEach((overflowItem, i) => {
+            overflowItem.style.top = remainingHeight * (numberLastItem + i + 1) + 'px';
+            contVerticalMenuUl.append(overflowItem);
 
-  document.addEventListener('keydown', e => {
-    if (e.key === 27 || e.keyCode === 27) {
-      closeVerticalMenu();
-      closeAllSubMenus();
+            if (i === overflowItemsLength - 1) {
+              heightContVerticalMenuOpen =
+                overflowItem.getBoundingClientRect().bottom -
+                contVerticalMenu.getBoundingClientRect().top;
+            }
+          });
+
+          contVerticalMenu.style.height = heightContVerticalMenuOpen + 'px';
+          menuItemOverflow.style.bottom = 'auto';
+          menuItemOverflow.style.top = heightContVerticalMenuOpen + 'px';
+        } else {
+          closeVerticalMenu();
+        }
+      } else if (
+        e.target !== contVerticalMenu &&
+        !e.target.classList.contains(classMenuItemDropIcon) &&
+        !e.target.classList.contains(classMenuItemDropIconOpen)
+      ) {
+        closeVerticalMenu();
+        closeAllSubMenus();
+      }
+    });
+
+    function closeVerticalMenu() {
+      if (!contVerticalMenu.classList.contains(classVerticalMenuOpen)) {
+        return;
+      }
+      contVerticalMenu.classList.remove(classVerticalMenuOpen);
+
+      for (let i = overflowItemsLength; i > 0; i--) {
+        menuOverflow.append(nl_menuItems[nl_menuItems.length - i]);
+      }
+
+      contVerticalMenu.style.height = heightContVerticalMenu + 'px';
+
+      if (menuItemOverflow) {
+        menuItemOverflow.style.top = 'auto';
+        menuItemOverflow.style.bottom = '0px';
+      }
     }
+
+    function closeSubMenu(menu) {
+      let dropIconOpen = menu
+        .closest('li')
+        .querySelector(`.${classMenuItemDropIconOpen}`);
+
+      dropIconOpen.classList.remove(classMenuItemDropIconOpen);
+      menu.classList.remove(classSubMenuIOpen);
+    }
+
+    function closeAllSubMenus() {
+      [...contVerticalMenu.querySelectorAll(`.${classSubMenuIOpen}`)]
+        .reverse()
+        .forEach(closeSubMenu);
+    }
+
+    document.addEventListener('keydown', e => {
+      if (e.key === 27 || e.keyCode === 27) {
+        closeVerticalMenu();
+        closeAllSubMenus();
+      }
+    });
   });
 });
--- app/src/template-parts/_templates/sections/hero-block-3/hero-block-3.php
+++ app/src/template-parts/_templates/sections/hero-block-3/hero-block-3.php
@@ -1,57 +1,57 @@
 <div class="rmbt-full-width rmbt-hero-block-3-full-width">
-	<section class="rmbt-container rmbt-hero-block-3">
+   <section class="rmbt-container rmbt-hero-block-3">
 
-		<div class="rmbt-hero-block-3__row">
+      <div class="rmbt-hero-block-3__row">
 
-			<div class='rmbt-hero-block-3__col rmbt-hero-block-3-col-left'>
-				<?php  // get_template_part( 'template-parts/_templates/components/rmbt-vertical-menu-0/rmbt-vertical-menu-0' ); ?>
-			</div>
+         <div class='rmbt-hero-block-3__col rmbt-hero-block-3-col-left'>
+            <?php   get_template_part( 'template-parts/_templates/components/rmbt-vertical-menu-0/rmbt-vertical-menu-0' ); ?>
+         </div>
 
-			<div class='rmbt-hero-block-3__col rmbt-hero-block-3-col-center'>
-				<div class="rmbt-hero-block-3__bg">
-					<div class="wrap-img">
-						<!-- <img src="<?php echo get_template_directory_uri() ?>/assets/img/bg_transparent.jpg"
+         <div class='rmbt-hero-block-3__col rmbt-hero-block-3-col-center'>
+            <div class="rmbt-hero-block-3__bg">
+               <div class="wrap-img">
+                  <!-- <img src="<?php echo get_template_directory_uri() ?>/assets/img/bg_transparent.jpg"
 							alt="hero-block-bg"> -->
-					</div>
-				</div>
-				<div class="rmbt-hero-block-3-text">
-					<div class="rmbt-hero-block-3-text__subtitle">
-						<div class="block-details">
-							subtitle
-						</div>
-					</div>
-					<div class="rmbt-hero-block-3-text__title">
-						<div class="block-details">
-							title
-							<p class='description'>https://wpbingosite.com/wordpress/davici/home-page-7/</p>
-						</div>
-					</div>
-					<?php get_template_part( 'template-parts/_templates/components/button-0/button-0' ); ?>
+               </div>
+            </div>
+            <div class="rmbt-hero-block-3-text">
+               <div class="rmbt-hero-block-3-text__subtitle">
+                  <div class="block-details">
+                     subtitle
+                  </div>
+               </div>
+               <div class="rmbt-hero-block-3-text__title">
+                  <div class="block-details">
+                     title
+                     <p class='description'>https://wpbingosite.com/wordpress/davici/home-page-7/</p>
+                  </div>
+               </div>
+               <?php get_template_part( 'template-parts/_templates/components/button-0/button-0' ); ?>
 
-				</div>
-			</div>
-			<div class='rmbt-hero-block-3__col rmbt-hero-block-3-col-right'>
+            </div>
+         </div>
+         <div class='rmbt-hero-block-3__col rmbt-hero-block-3-col-right'>
 
-				<div class="rmbt-hero-block-3-col-right__top">
-					<div class="rmbt-hero-block-3-col-right__bg">
-						<!-- <div class="wrap-img">
+            <div class="rmbt-hero-block-3-col-right__top">
+               <div class="rmbt-hero-block-3-col-right__bg">
+                  <!-- <div class="wrap-img">
 						<img src="<?php echo get_template_directory_uri() ?>/assets/img/bg_transparent.jpg"
 							alt="hero-block-bg">
 					</div> -->
-					</div>
-				</div>
-				<div class="rmbt-hero-block-3-col-right__bottom">
-					<div class="rmbt-hero-block-3-col-right__bg">
-						<!-- <div class="wrap-img">
+               </div>
+            </div>
+            <div class="rmbt-hero-block-3-col-right__bottom">
+               <div class="rmbt-hero-block-3-col-right__bg">
+                  <!-- <div class="wrap-img">
 						<img src="<?php echo get_template_directory_uri() ?>/assets/img/bg_transparent.jpg"
 							alt="hero-block-bg">
 					</div> -->
-					</div>
-				</div>
+               </div>
+            </div>
 
 
 
-			</div>
-		</div>
-	</section>
+         </div>
+      </div>
+   </section>
 </div>

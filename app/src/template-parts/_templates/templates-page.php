5a71e427797c6637fb33fe2ecc13cb0dba0d5b14
Rombt <rombtnet@gmail.com>
2024-09-16 13:42:18 +0300

    .

--- app/src/template-parts/_templates/templates-page.php
+++ app/src/template-parts/_templates/templates-page.php
@@ -3,44 +3,45 @@
 
 
 <main>
-	<style>
-		.templates-page-section-title {
-			width: 100%;
-		}
+   <style>
+   .templates-page-section-title {
+      width: 100%;
+   }
 
-		.templates-page-section-title h2 {
-			margin-top: 0px;
-			margin-bottom: 0px;
-		}
+   .templates-page-section-title h2 {
+      margin-top: 0px;
+      margin-bottom: 0px;
+   }
 
-		.templates-page-section p {
-			margin-top: 5px;
-			margin-bottom: 5px;
-		}
+   .templates-page-section p {
+      margin-top: 5px;
+      margin-bottom: 5px;
+   }
 
-		.templates-page-components {}
+   .templates-page-components {
 
-		.templates-page-components p {
-			margin-bottom: 30px;
+      width: 100%;
+   }
 
-		}
+   .templates-page-components p {
+      margin-bottom: 30px;
 
-		.templates-page-components__body {
+   }
 
-			/* padding: 20px 20px 20px 20px; */
-			display: flex;
-			row-gap: 15px;
+   .templates-page-components__body {
 
-			height: 200px;
-			border: 1px solid #0515fd;
+      display: flex;
+      row-gap: 15px;
 
-
-
-		}
-	</style>
+      /* height: 250px; */
+      /* border: 1px solid #9c9c9c; */
 
 
-	<?php $arr_dirs = scandir( __DIR__, SCANDIR_SORT_DESCENDING );
+   }
+   </style>
+
+
+   <?php $arr_dirs = scandir( __DIR__, SCANDIR_SORT_DESCENDING );
 
 	$number_active_tab = 1;
 	$templates_types = [];
@@ -62,67 +63,67 @@
 
 
 
-	<div class="tabs rmbt-templates-page-tabs">
-		<nav data-tabs-titles class="tabs__nav">
-			<?php
+   <div class="tabs rmbt-templates-page-tabs">
+      <nav data-tabs-titles class="tabs__nav">
+         <?php
 			$i = 0;
 			foreach ( $templates_types as $template_type_name => $template_type_value ) : ?>
-				<?php if ( $i === $number_active_tab ) : ?>
-					<button type="button" class="tabs__title tabs__title-active"
-						data-tab="<?php echo $template_type_name; ?>"><?php echo $template_type_name; ?>
-					</button>
-				<?php else : ?>
-					<button type="button" class="tabs__title"
-						data-tab="<?php echo $template_type_name; ?>"><?php echo $template_type_name; ?></button>
-				<?php endif ?>
-				<?php $i++; ?>
-			<?php endforeach ?>
-		</nav>
+         <?php if ( $i === $number_active_tab ) : ?>
+         <button type="button" class="tabs__title tabs__title-active"
+            data-tab="<?php echo $template_type_name; ?>"><?php echo $template_type_name; ?>
+         </button>
+         <?php else : ?>
+         <button type="button" class="tabs__title"
+            data-tab="<?php echo $template_type_name; ?>"><?php echo $template_type_name; ?></button>
+         <?php endif ?>
+         <?php $i++; ?>
+         <?php endforeach ?>
+      </nav>
 
-		<div class="tabs__content">
-			<?php $i = 0;
+      <div class="tabs__content">
+         <?php $i = 0;
 			foreach ( $templates_types as $template_type => $arr_templates ) {
 				if ( $i === $number_active_tab ) { ?>
-					<div class="tabs__body tabs__body-active" data-tab-name="<?php echo $template_type ?>">
-					<?php } else { ?>
-						<div class="tabs__body" data-tab-name="<?php echo $template_type ?>">
-						<?php } ?>
+         <div class="tabs__body tabs__body-active" data-tab-name="<?php echo $template_type ?>">
+            <?php } else { ?>
+            <div class="tabs__body" data-tab-name="<?php echo $template_type ?>">
+               <?php } ?>
 
-						<?php foreach ( $arr_templates as $template ) { ?>
-
-
-							<?php if ( $template_type === 'components' ) : ?>
-								<div class="rmbt-container templates-page-components">
-									<h2><?php echo $template; ?></h2>
-									<p>get_template_part('<?php echo 'template-parts/_templates/' . $template_type . DIRECTORY_SEPARATOR . $template . DIRECTORY_SEPARATOR . $template ?>');
-									</p>
-
-									<div class="templates-page-components__body">
-
-										<?php get_template_part( 'template-parts/_templates/' . $template_type . DIRECTORY_SEPARATOR . $template . DIRECTORY_SEPARATOR . $template ); ?>
-									</div>
-								</div>
+               <?php foreach ( $arr_templates as $template ) { ?>
 
 
-							<?php else : ?>
+               <?php if ( $template_type === 'components' ) : ?>
+               <div class="rmbt-container templates-page-components">
+                  <h2><?php echo $template; ?></h2>
+                  <p>get_template_part('<?php echo 'template-parts/_templates/' . $template_type . DIRECTORY_SEPARATOR . $template . DIRECTORY_SEPARATOR . $template ?>');
+                  </p>
+
+                  <div class="templates-page-components__body">
+
+                     <?php get_template_part( 'template-parts/_templates/' . $template_type . DIRECTORY_SEPARATOR . $template . DIRECTORY_SEPARATOR . $template ); ?>
+                  </div>
+               </div>
 
 
-								<div class="rmbt-container templates-page-section-title">
-									<h2><?php echo $template; ?></h2>
-									<p>get_template_part('<?php echo 'template-parts/_templates/' . $template_type . DIRECTORY_SEPARATOR . $template . DIRECTORY_SEPARATOR . $template ?>');
-									</p>
-								</div>
-
-								<?php get_template_part( 'template-parts/_templates/' . $template_type . DIRECTORY_SEPARATOR . $template . DIRECTORY_SEPARATOR . $template ); ?>
-							<?php endif ?>
+               <?php else : ?>
 
 
-						<?php } ?>
-					</div>
-					<?php $i++;
+               <div class="rmbt-container templates-page-section-title">
+                  <h2><?php echo $template; ?></h2>
+                  <p>get_template_part('<?php echo 'template-parts/_templates/' . $template_type . DIRECTORY_SEPARATOR . $template . DIRECTORY_SEPARATOR . $template ?>');
+                  </p>
+               </div>
+
+               <?php get_template_part( 'template-parts/_templates/' . $template_type . DIRECTORY_SEPARATOR . $template . DIRECTORY_SEPARATOR . $template ); ?>
+               <?php endif ?>
+
+
+               <?php } ?>
+            </div>
+            <?php $i++;
 			} ?>
-			</div>
-		</div>
+         </div>
+      </div>
 
 
 

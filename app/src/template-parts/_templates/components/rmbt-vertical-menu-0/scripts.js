c15f7c272f80d92659c25d7cfb60439977dfcb90
Rombt <rombtnet@gmail.com>
2024-09-21 19:22:20 +0300

    .

--- app/src/template-parts/_templates/components/rmbt-vertical-menu-0/scripts.js
+++ app/src/template-parts/_templates/components/rmbt-vertical-menu-0/scripts.js
@@ -1,6 +1,4 @@
 class VerticalMenu {
-  // todo если меню перестраивается в методе initParamMenus() меню нужно скрыть и показывать только после окончания построения
-
   contVerticalMenu;
   nl_menuItems;
   rectContVerticalMenu;

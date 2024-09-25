9e01b5cca7c6b4b3c8ff9a08de464f2123cca2cb
Rombt <rombtnet@gmail.com>
2024-09-25 20:53:38 +0300

    ++

--- app/src/template-parts/_templates/sections/section-tabs-1/section-tabs-1.php
+++ app/src/template-parts/_templates/sections/section-tabs-1/section-tabs-1.php
@@ -6,16 +6,31 @@
 
       <div class="tabs rmbt-section-tabs-1-tabs">
          <nav data-tabs-titles class="tabs__nav">
-            <div class="tabs__title tabs__title-active" data-tab="tab-1"></div>
-            <div class="tabs__title" data-tab="tab-2"></div>
-            <div class="tabs__title" data-tab="tab-3"></div>
+            <div class="tabs__title tabs__title-active" data-tab="tab-1">
+               title-tab-1
+            </div>
+            <div class="tabs__title" data-tab="tab-2">
+               title-tab-2
+            </div>
+            <div class="tabs__title" data-tab="tab-3">
+               title-tab-3
+            </div>
          </nav>
          <div class="tabs__content">
             <div class="tabs__body tabs__body-active" data-tab-name="tab-1">
+               <div class="block-details">
+                  tab-1
+               </div>
             </div>
             <div class="tabs__body" data-tab-name="tab-2">
+               <div class="block-details">
+                  tab-2
+               </div>
             </div>
             <div class="tabs__body" data-tab-name="tab-3">
+               <div class="block-details">
+                  tab-3
+               </div>
             </div>
          </div>
       </div>

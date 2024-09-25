7ce7f6716034f4c93eaa68edeed788b94cb79f89
Rombt <rombtnet@gmail.com>
2024-09-25 19:49:52 +0300

    improve all tabs

---
+++ app/src/template-parts/_templates/sections/section-tabs-1/section-tabs-1.php
@@ -0,0 +1,28 @@
+<div class="rmbt-full-width rmbt-section-tabs-1-full-width">
+	<section class="rmbt-container rmbt-section-tabs-1">
+
+		<div class="rmbt-section-tabs-1__row">
+
+
+			<div class="tabs rmbt-section-tabs-1-tabs">
+				<nav data-tabs-titles class="tabs__nav">
+					<button type="button" class="tabs__title tabs__title-active" data-tab="tab-1"></button>
+					<button type="button" class="tabs__title" data-tab="tab-2"></button>
+					<button type="button" class="tabs__title" data-tab="tab-3"></button>
+				</nav>
+				<div class="tabs__content">
+					<div class="tabs__body tabs__body-active" data-tab-name="tab-1">
+
+					</div>
+					<div class="tabs__body" data-tab-name="tab-2">
+
+					</div>
+					<div class="tabs__body" data-tab-name="tab-3">
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</section>
+</div>

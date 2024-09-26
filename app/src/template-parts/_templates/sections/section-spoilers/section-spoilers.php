a8411f8b9f831207518fe7ce27a441dfdbc0800d
Rombt <rombtnet@gmail.com>
2024-09-26 20:05:07 +0300

    .

---
+++ app/src/template-parts/_templates/sections/section-spoilers/section-spoilers.php
@@ -0,0 +1,35 @@
+<div class="rmbt-full-width rmbt-section-spoilers-full-width">
+	<section class="rmbt-container rmbt-section-spoilers">
+		<div class="rmbt-section-spoilers-head">
+			<!-- <div class="rmbt-section-spoilers__head"> -->
+			<h2 class='rmbt-section-spoilers-head__title'>
+				<div class="block-details"> title </div>
+				<?php // echo rmbt_get_redux_field( 'rmbt-section-spoilers_section-title' ) ?>
+				<?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt-_section-title')]); ?>
+			</h2>
+			<p class='rmbt-section-spoilers-head__subtitle block-details'>
+				subtitle
+				<?php // echo rmbt_get_redux_field( 'rmbt-section-spoilers_section-text' ) ?>
+			</p>
+		</div>
+		<div class="rmbt-section-spoilers__row">
+
+			<div class='spoilers rmbt-section-spoilers__left-col'>
+				<div class='spoilers__item'>
+					<div class='spoilers__title'>Spoiler Title 1</div>
+					<div class='spoilers__body'>Spoiler content 1</div>
+				</div>
+			</div>
+
+			<div class='rmbt-section-spoilers__right-col '>
+				<div class="wrap-img">
+					<!-- <img src="<?php echo get_template_directory_uri() ?>/assets/img/" alt=""> -->
+				</div>
+
+
+			</div>
+		</div>
+
+
+	</section>
+</div>

4942541eeccedbea24b9d09ef51ca580884e850d
Rombt <rombtnet@gmail.com>
2024-09-26 15:40:10 +0300

    block-0 and block-1 are ready

--- app/src/template-parts/_templates/blocks/block-0/block-0.php
+++ app/src/template-parts/_templates/blocks/block-0/block-0.php
@@ -1,27 +1,13 @@
-<div class="rmbt-full-width rmbt-simple-section-full-width">
-	<div class="simple-section-full-width__bg">
-		<div class="wrap-img">
-			<!-- <img src="<?php echo get_template_directory_uri() ?>/assets/img/simple_bg.png" alt="simple_bg"> -->
-		</div>
-	</div>
-	<section class="rmbt-container rmbt-simple-section">
-		<!-- <div class="rmbt-simple-section__row"> -->
+<div class="rmbt-block-0">
+	<h3 class='rmbt-block-0__head block-details'>
+		title
+		<?php // echo rmbt_get_redux_field( 'rmbt_block-0_title' ) ?>
+		<?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt_block-0_title')]); ?>
+	</h3>
+	<p class='rmbt-block-0__body block-details'>
+		text
+		<?php // echo rmbt_get_redux_field( 'rmbt_block-0_text' ) ?>
+	</p>
 
-		<div class="rmbt-simple-section__row rmbt-simple-section-head">
-			<h2 class='rmbt-simple-section-head__title'>
-				<div class="block-details"> title </div>
-				<?php // echo rmbt_get_redux_field( 'rmbt-simple-section_section-title' ) ?>
-				<?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt-_section-title')]); ?>
-			</h2>
-			<p class='rmbt-simple-section-head__subtitle block-details'>
-				subtitle
-				<?php // echo rmbt_get_redux_field( 'rmbt-simple-section_section-text' ) ?>
-			</p>
-		</div>
 
-		<div class="rmbt-simple-section__row rmbt-simple-section-body">
-			<div class="block-details"> other content</div>
-		</div>
-		<!-- </div> -->
-	</section>
 </div>

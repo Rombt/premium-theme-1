2dc7ea8db2c4270e0f128238d79f1e7e8ad27ee9
Rombt <rombtnet@gmail.com>
2024-09-26 16:21:31 +0300

    .

--- app/src/assets/js/modules/spoiler.js
+++ app/src/assets/js/modules/spoiler.js
@@ -6,37 +6,43 @@
  */
 
 function spoiler() {
+  const spoilers = document.querySelectorAll('.spoilers__title');
 
-    const spoilers = document.querySelectorAll('.spoiler__title');
+  spoilers.forEach(spoiler => {
+    spoiler.addEventListener('click', e => {
+      const spoilerBody = e.target
+        .closest('.spoilers__item')
+        .querySelector('.spoilers__body');
 
-    spoilers.forEach(spoiler => {
-        spoiler.addEventListener('click', e => {
-            spoilerText = e.target.closest('.spoiler').querySelector('.spoiler__text');
-            spoilerText.classList.toggle('open');
-            e.target.classList.toggle('spoiler__title_open');
-            if (spoilerText.classList.contains('open')) {
-                if (spoilerText) {
-                    gsap.to(spoilerText, {
-                        duration: 0.5,
-                        ease: "power1.in",
-                        height: 'auto',
-                        overflow: 'visible',
-                        pointerEvents: 'auto',
-                        opacity: 1,
-                    });
-                }
-            } else {
-                gsap.to(spoilerText, {
-                    duration: 0.5,
-                    ease: "power1.in",
-                    height: '0px',
-                    overflow: 'hidden',
-                    pointerEvents: 'none',
-                    opacity: 0,
-                });
-            }
-        })
-    })
+      console.log('spoilerBody = ', spoilerBody);
+      spoilerBody.classList.toggle('spoilers__body_open');
+      e.target.classList.toggle('spoilers__title_open');
+
+      if (typeof gsap !== 'undefined') {
+        if (spoilerBody.classList.contains('open')) {
+          if (spoilerBody) {
+            gsap.to(spoilerBody, {
+              duration: 0.5,
+              ease: 'power1.in',
+              height: 'auto',
+              overflow: 'visible',
+              pointerEvents: 'auto',
+              opacity: 1,
+            });
+          }
+        } else {
+          gsap.to(spoilerBody, {
+            duration: 0.5,
+            ease: 'power1.in',
+            height: '0px',
+            overflow: 'hidden',
+            pointerEvents: 'none',
+            opacity: 0,
+          });
+        }
+      }
+    });
+  });
 }
 
 spoiler();
---
+++ app/src/template-parts/_templates/blocks/block-spoilers/block-spoilers.php
@@ -0,0 +1,19 @@
+<article class="rmbt-block-spoilers">
+   <h3 class='rmbt-block-spoilers__head block-details'>
+      title
+      <?php // echo rmbt_get_redux_field( 'rmbt_block-spoilers_title' ) ?>
+      <?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt_block-spoilers_title')]); ?>
+   </h3>
+   <div class='rmbt-block-spoilers__body block-details'>
+
+      <div data-spoilers class="spoilers">
+         <div class="spoilers__item">
+            <button type="button" data-spoiler class="spoilers__title">Spoiler Title 1</button>
+            <div class="spoilers__body">Spoiler content 1</div>
+         </div>
+      </div>
+
+   </div>
+
+
+</article>

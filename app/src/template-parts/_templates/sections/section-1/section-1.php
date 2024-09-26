080b668ebcd594308ebe2e4fabdc2ae03fad2929
Rombt <rombtnet@gmail.com>
2024-09-26 13:43:12 +0300

    section-2 is ready

--- app/src/template-parts/_templates/sections/section-1/section-1.php
+++ app/src/template-parts/_templates/sections/section-1/section-1.php
@@ -2,9 +2,34 @@
 	<section class="rmbt-container rmbt-section-1">
 
 		<div class="rmbt-section-1__row">
-			<div class="rmbt-section-1__col">
-				<div class="rmbt-section-1-head">
-					<h2 class='rmbt-section-1-head__title'>
+			<div class="rmbt-section-1__col rmbt-section-1-video">
+				<!-- <div class="block-details"> video </div> -->
+
+			</div>
+			<div class="rmbt-section-1__col rmbt-section-1-text">
+				<h2 class='rmbt-section-1-text__title'>
+					title
+					<?php // echo rmbt_get_redux_field( 'rmbt-section-1_section-title' ) ?>
+					<?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt-_section-title')]); ?>
+				</h2>
+				<p class='rmbt-section-1-text__subtitle'>
+					subtitle
+					<?php // echo rmbt_get_redux_field( 'rmbt-section-1_section-text' ) ?>
+				</p>
+				<p class='rmbt-section-1-text__text'>
+					text
+					<?php // echo rmbt_get_redux_field( 'rmbt-section-1_section-text' ) ?>
+				</p>
+
+			</div>
+		</div>
+
+
+	</section>
+</div>
+
+
+<!-- <h2 class='rmbt-section-1-head__title'>
 						<div class="block-details"> title </div>
 						<?php // echo rmbt_get_redux_field( 'rmbt-section-1_section-title' ) ?>
 						<?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt-_section-title')]); ?>
@@ -12,27 +37,9 @@
 					<p class='rmbt-section-1-head__subtitle block-details'>
 						subtitle
 						<?php // echo rmbt_get_redux_field( 'rmbt-section-1_section-text' ) ?>
-					</p>
-				</div>
+					</p> -->
 
-				<div class="rmbt-section-1__content">
-					<div class="rmbt-section-1__text">
-						<p class='text-block__text block-details'></p>
-						<p class='text-block__author block-details'>
-							<span>author name</span>
-							about author
-						</p>
-					</div>
-					<div class="wrap-img">
-						<!-- <img src="<?php echo get_template_directory_uri() ?>/assets/img/" alt=""> -->
-					</div>
 
-				</div>
-			</div>
-		</div>
-
-		<!-- <div class="rmbt-section-1__row rmbt-section-1-body">
+<!-- <div class="rmbt-section-1__row rmbt-section-1-body">
 			<div class="block-details"> other content</div>
 		</div> -->
-	</section>
-</div>
---
+++ app/src/template-parts/_templates/sections/section-2/section-2.php
@@ -0,0 +1,33 @@
+<div class="rmbt-full-width rmbt-section-2-full-width">
+   <section class="rmbt-container rmbt-section-2">
+
+      <div class="rmbt-section-2__row">
+         <div class="rmbt-section-2__col">
+            <div class="rmbt-section-2-head">
+               <h2 class='rmbt-section-2-head__title'>
+                  <div class="block-details"> title </div>
+                  <?php // echo rmbt_get_redux_field( 'rmbt-section-2_section-title' ) ?>
+                  <?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt-_section-title')]); ?>
+               </h2>
+               <p class='rmbt-section-2-head__subtitle block-details'>
+                  subtitle
+                  <?php // echo rmbt_get_redux_field( 'rmbt-section-2_section-text' ) ?>
+               </p>
+            </div>
+
+            <div class="rmbt-section-2__content">
+               <div class="rmbt-section-2__text">
+                  <p class='text-block__text block-details'>text</p>
+               </div>
+               <div class="wrap-img">
+                  <div class="block-details">
+                     img
+                  </div>
+                  <!-- <img src="<?php echo get_template_directory_uri() ?>/assets/img/" alt=""> -->
+               </div>
+
+            </div>
+         </div>
+      </div>
+   </section>
+</div>

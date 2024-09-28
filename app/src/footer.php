4393c6c59ccc055233539a169f9444e5646d0ecd
Rombt <rombtnet@gmail.com>
2024-09-28 18:22:12 +0300

    .

--- app/src/footer.php
+++ app/src/footer.php
@@ -1,5 +1,8 @@
-</div> <!--  end rmbt-page-wrap  -->
 <?php wp_footer(); ?>
+
+<?php get_template_part( 'pages\_templates\footers\footer-0\footer-0', '0' ); ?>
+</div> <!--  end rmbt-page-wrap  -->
+
 </body>
 
 </html>
--- app/src/front-page.php
+++ app/src/front-page.php
@@ -3,9 +3,10 @@
 
 
 <main>
-	<?php
-	get_template_part( 'pages\_templates\headers\header-0\header-0', '0' );
+   <?php
+
 	get_template_part( 'pages\_templates\sections\hero_block_3\hero_block_3', null );
+	
 	?>
 </main>
 

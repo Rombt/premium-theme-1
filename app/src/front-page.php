1920f4649ee639bf1f24f9e8fcc60a5efedfcd85
Rombt <rombtnet@gmail.com>
2024-09-28 15:56:53 +0300

    .

--- app/src/front-page.php
+++ app/src/front-page.php
@@ -4,14 +4,9 @@
 
 <main>
 	<?php
-	get_template_part( 'template-parts/_templates/headers\header-1\header-1', '1' );
 
-	get_template_part( 'template-parts/_templates/sections\hero_block_2\hero_block_2', null );
-
-	get_template_part( 'template-parts/_templates/sections\section-spoilers\section-spoilers', 'spoilers' );
-
-	// get_template_part( 'template-parts/_templates/headers\header-2\header-2', '2' );
-	// get_template_part( 'template-parts/_templates/headers\header-3\header-3', '3' );
+	get_template_part( 'pages\_templates\headers\header-3\header-3', '3' );
+	get_template_part( 'pages\_templates\sections\hero_block_3\hero_block_3', null );
 	?>
 </main>
 

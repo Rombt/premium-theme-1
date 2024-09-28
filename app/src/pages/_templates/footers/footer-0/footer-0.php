875010459555dff28b169bc27355ecc4bfc50c23
Rombt <rombtnet@gmail.com>
2024-09-28 17:27:44 +0300

    .

---
+++ app/src/pages/_templates/footers/footer-0/footer-0.php
@@ -0,0 +1,19 @@
+<div class="rmbt-full-width rmbt-footer-0-full-width">
+   <section class="rmbt-container rmbt-footer-0">
+      <div class="rmbt-footer-0__row rmbt-footer-0-row-content">
+         <div class="rmbt-footer-0__col">
+            <div class="block-details"> other content</div>
+         </div>
+         <div class="rmbt-footer-0__col">
+            <div class="block-details"> other content</div>
+         </div>
+         <div class="rmbt-footer-0__col">
+            <div class="block-details"> other content</div>
+         </div>
+      </div>
+
+
+      <div class="rmbt-footer-0__row rmbt-footer-0-row-copyright"></div>
+
+   </section>
+</div>

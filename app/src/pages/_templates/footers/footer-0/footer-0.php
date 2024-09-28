c8eff50d115ccfc0a27dad4d9c24057bdca3d13c
Rombt <rombtnet@gmail.com>
2024-09-28 18:07:08 +0300

    footer-0 is ready

--- app/src/pages/_templates/footers/footer-0/footer-0.php
+++ app/src/pages/_templates/footers/footer-0/footer-0.php
@@ -1,19 +1,22 @@
 <div class="rmbt-full-width rmbt-footer-0-full-width">
-   <section class="rmbt-container rmbt-footer-0">
-      <div class="rmbt-footer-0__row rmbt-footer-0-row-content">
-         <div class="rmbt-footer-0__col">
-            <div class="block-details"> other content</div>
-         </div>
-         <div class="rmbt-footer-0__col">
-            <div class="block-details"> other content</div>
-         </div>
-         <div class="rmbt-footer-0__col">
-            <div class="block-details"> other content</div>
-         </div>
-      </div>
+	<section class="rmbt-container rmbt-footer-0">
+		<div class="rmbt-footer-0__row rmbt-footer-0-row-content">
+			<div class="rmbt-footer-0__col">
+				<div class="block-details"> other content</div>
+			</div>
+			<div class="rmbt-footer-0__col">
+				<div class="block-details"> other content</div>
+			</div>
+			<div class="rmbt-footer-0__col">
+				<div class="block-details"> other content</div>
+			</div>
+		</div>
 
 
-      <div class="rmbt-footer-0__row rmbt-footer-0-row-copyright"></div>
+		<div class="rmbt-footer-0__row rmbt-footer-0-row-copyright">
+			<?php get_template_part( 'pages\_templates\blocks\block-copyright-0\block-copyright-0', 'copyright-0' ); ?>
 
-   </section>
+		</div>
+
+	</section>
 </div>

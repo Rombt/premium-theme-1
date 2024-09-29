d5afc96d5c7b3c1918be24416a0e1baae43a05f0
Rombt <rombtnet@gmail.com>
2024-09-29 17:28:16 +0300

    ++

--- app/src/front-page.php
+++ app/src/front-page.php
@@ -3,10 +3,9 @@
 
 
 <main>
-   <?php
-
+	<?php
 	get_template_part( 'pages\_templates\sections\hero_block_3\hero_block_3', null );
-	
+	get_template_part( 'pages\_templates\sections\section_cards-0\section_cards-0', '0' );
 	?>
 </main>
 

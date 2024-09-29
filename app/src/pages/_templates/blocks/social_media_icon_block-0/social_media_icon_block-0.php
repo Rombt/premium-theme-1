05b66fc4e3498cdac1160a9605513eb90047d2a2
Rombt <rombtnet@gmail.com>
2024-09-29 12:34:01 +0300

    social_media_icon_block-0 is ready

--- app/src/pages/_templates/blocks/social_media_icon_block-0/social_media_icon_block-0.php
+++ app/src/pages/_templates/blocks/social_media_icon_block-0/social_media_icon_block-0.php
@@ -1,13 +1,25 @@
-<article class="rmbt-block-0">
-	<h3 class='rmbt-block-0__head block-details'>
-		title
-		<?php // echo rmbt_get_redux_field( 'rmbt_block-0_title' ) ?>
-		<?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt_block-0_title')]); ?>
-	</h3>
-	<p class='rmbt-block-0__body block-details'>
-		text
-		<?php // echo rmbt_get_redux_field( 'rmbt_block-0_text' ) ?>
-	</p>
+<div class="rmbt-social-media-icon-block-0">
+
+   <div class='rmbt-social-media-icon-block-0__icon'>
+      <svg>
+         <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#"></use>
+      </svg>
+   </div>
+   <div class='rmbt-social-media-icon-block-0__icon'>
+      <svg>
+         <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#"></use>
+      </svg>
+   </div>
+   <div class='rmbt-social-media-icon-block-0__icon'>
+      <svg>
+         <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#"></use>
+      </svg>
+   </div>
+   <div class='rmbt-social-media-icon-block-0__icon'>
+      <svg>
+         <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#"></use>
+      </svg>
+   </div>
 
 
-</article>
+</div>

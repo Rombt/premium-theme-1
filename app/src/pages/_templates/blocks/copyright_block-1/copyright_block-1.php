c1162b88301a0df2ff590265322959135957237c
Rombt <rombtnet@gmail.com>
2024-09-29 12:56:23 +0300

    .

---
+++ app/src/pages/_templates/blocks/copyright_block-1/copyright_block-1.php
@@ -0,0 +1,10 @@
+<div class="rmbt-copyright-block-1">
+	<div class='rmbt-copyright-block-1__body'>
+		2024 Rombt Net Studio
+		<svg>
+			<use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#copyright_1"></use>
+		</svg>
+		All rights reserved
+		<?php // echo rmbt_get_redux_field( 'rmbt_copyright-block-0_text' ) ?>
+	</div>
+</div>

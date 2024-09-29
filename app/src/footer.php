19cd768822f1a6a563b73c1327de50c1dd8c195f
Rombt <rombtnet@gmail.com>
2024-09-29 16:37:50 +0300

    ..

--- app/src/footer.php
+++ app/src/footer.php
@@ -1,6 +1,8 @@
 <?php wp_footer(); ?>
 
-<?php get_template_part( 'pages\_templates\footers\footer-0\footer-0', '0' ); ?>
+<?php // get_template_part( 'pages\_templates\footers\footer-0\footer-0', '0' ); ?>
+<?php get_template_part( 'pages\_templates\footers\footer-1\footer-1', '1' ); ?>
+
 </div> <!--  end rmbt-page-wrap  -->
 
 </body>

4548db4edb21e0ec3e7f259a1bc1f9006de6be4d
Rombt <rombtnet@gmail.com>
2024-09-30 12:17:31 +0300

    .

--- app/src/pages/_templates/footers/footer-2/footer-2.php
+++ app/src/pages/_templates/footers/footer-2/footer-2.php
@@ -1,20 +1,18 @@
-<div class="rmbt-full-width rmbt-footer-0-full-width">
+<div class="rmbt-full-width rmbt-footer-2-full-width">
 	<section class="rmbt-container rmbt-footer-2">
-		<div class="rmbt-footer-0__row rmbt-footer-0-row-content">
-			<div class="rmbt-footer-0__col">
+		<div class="rmbt-footer-2__row rmbt-footer-2-row-content">
+			<div class="rmbt-footer-2__col rmbt-footer-2-left-col">
+				<div class="rmbt-footer-2__row rmbt-footer-2-row-copyright">
+					<?php get_template_part( 'pages\_templates\blocks\copyright_block-1\copyright_block-1', '1' ); ?>
+				</div>
+			</div>
+			<div class="rmbt-footer-2__col rmbt-footer-2-center-col">
 				<div class="block-details"> footer content</div>
 			</div>
-			<div class="rmbt-footer-0__col">
-				<div class="block-details"> footer content</div>
-			</div>
-			<div class="rmbt-footer-0__col">
-				<div class="block-details"> footer content</div>
-			</div>
-		</div>
+			<div class="rmbt-footer-2__col rmbt-footer-2-right-col">
+				<?php get_template_part( 'pages\_templates\blocks\social_media_icon_block-0\social_media_icon_block-0', '0' ); ?>
 
-
-		<div class="rmbt-footer-0__row rmbt-footer-0-row-copyright">
-			<?php get_template_part( 'pages\_templates\blocks\copyright_block-0\copyright_block-0', '0' ); ?>
+			</div>
 		</div>
 
 	</section>

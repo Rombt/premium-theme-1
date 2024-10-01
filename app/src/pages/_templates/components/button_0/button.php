93f7cdc73baf5cdf5f2409de6b3c4b2fe471aced
Rombt <rombtnet@gmail.com>
2024-10-01 21:17:36 +0300

    .

---
+++ app/src/pages/_templates/components/button_0/button.php
@@ -0,0 +1,3 @@
+<a href="<?php echo $args['href']; ?>" class='rmbt-button <?php echo $class['class']; ?>'>
+   <?php echo ! empty( $args['text'] ) ? $args['text'] : 'simple button'; ?>
+</a>
--- app/src/pages/_templates/components/button_0/button_0.php
+++
@@ -1,3 +0,0 @@
-<a href="<?php echo $args['href']; ?>" class='rmbt-button-0 <?php echo $class['href']; ?>'>
-   <?php echo ! empty( $args['text'] ) ? $args['text'] : 'simple button'; ?>
-</a>

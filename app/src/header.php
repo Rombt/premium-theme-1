5c2859f6ae7cb03875c12f5a5729e9a599cb7151
Rombt <rombtnet@gmail.com>
2024-11-27 13:15:06 +0200

    1

--- app/src/header.php
+++ app/src/header.php
@@ -2,21 +2,21 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-   <meta charset="<?php bloginfo( 'charset' ); ?>">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="profile" href="https://gmpg.org/xfn/11">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-   <?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-   <?php wp_body_open(); ?>
+	<?php wp_body_open(); ?>
 
-   <?php // get_template_part( 'pages\_templates\components\debug-grid' ); ?>
+	<?php // get_template_part( 'pages\_templates\components\debug-grid' ); ?>
 
-   <?php if ( is_page( 'Страница шаблонов' ) ) : ?>
-   <div class="rmbt-page-wrap templates-page-wrap">
-      <?php elseif ( is_front_page() ) : ?>
-      <div class="rmbt-page-wrap front-page-wrap">
-         <?php get_template_part( 'pages\_templates\headers\header-2\header-2', '0' ); ?>
-         <?php endif ?>
+	<?php if ( is_page( 'Страница шаблонов' ) ) : ?>
+		<div class="rmbt-page-wrap templates-page-wrap">
+		<?php elseif ( is_front_page() ) : ?>
+			<div class="rmbt-page-wrap front-page-wrap">
+				<?php get_template_part( 'pages/parts/header-2/header-2', '0' ); ?>
+			<?php endif ?>

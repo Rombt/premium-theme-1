53275d789c3591061826a8a5b41b42d093c42da4
Rombt <rombtnet@gmail.com>
2024-11-27 15:17:36 +0200

    .

--- app/src/pages/_templates/sections/hero_block_1/hero_block_1.php
+++ app/src/pages/_templates/sections/hero_block_1/hero_block_1.php
@@ -1,10 +1,10 @@
-<div class="rmbt-full-width rmbt-hero-block-1-full-width">
-	<section class="rmbt-container rmbt-hero-block-1">
+<div class="rmbt-full-width _rmbt-hero-block-1-full-width">
+	<section class="rmbt-container _rmbt-hero-block-1">
 
-		<div class="rmbt-hero-block-1__row">
-			<div class='rmbt-hero-block-1__col rmbt-hero-block-1-col-right'>
+		<div class="_rmbt-hero-block-1__row">
+			<div class='_rmbt-hero-block-1__col _rmbt-hero-block-1-col-right'>
 
-				<div class="rmbt-hero-block-1-col-right__bg">
+				<div class="_rmbt-hero-block-1-col-right__bg">
 					<!-- <div class="wrap-img">
 						<img src="<?php echo get_template_directory_uri() ?>/assets/img/bg_transparent.jpg"
 							alt="hero-block-bg">
@@ -13,9 +13,9 @@
 
 
 			</div>
-			<div class='rmbt-hero-block-1__col rmbt-hero-block-1-col-left'>
+			<div class='_rmbt-hero-block-1__col _rmbt-hero-block-1-col-left'>
 
-				<div class="rmbt-hero-block-1-col-left__bg">
+				<div class="_rmbt-hero-block-1-col-left__bg">
 					<!-- <div class="wrap-img">
 						<img src="<?php echo get_template_directory_uri() ?>/assets/img/bg_transparent.jpg"
 							alt="hero-block-bg">
@@ -23,21 +23,21 @@
 				</div>
 
 			</div>
-			<div class="rmbt-hero-block-1__title-block rmbt-title-block">
+			<div class="_rmbt-hero-block-1__title-block _rmbt-title-block">
 
-				<!-- <div class="rmbt-title-block__decor"></div> -->
-				<div class="rmbt-title-block__icon"></div>
-				<div class="rmbt-title-block__subtitle">
+				<!-- <div class="_rmbt-title-block__decor"></div> -->
+				<div class="_rmbt-title-block__icon"></div>
+				<div class="_rmbt-title-block__subtitle">
 					<div class="block-details">
 						subtitle
 					</div>
 				</div>
-				<div class="rmbt-title-block__title">
+				<div class="_rmbt-title-block__title">
 					<div class="block-details">
 						title
 					</div>
 				</div>
-				<!-- <a href="#" class='rmbt-title-block__button'>
+				<!-- <a href="#" class='_rmbt-title-block__button'>
 					<div class="block-details">
 						cal-to-action-bottom
 					</div>
@@ -46,7 +46,7 @@
 
 
 			</div>
-			<div class="rmbt-hero-block-1__right-text">
+			<div class="_rmbt-hero-block-1__right-text">
 				<div class="block-details">
 					текстовой нюанс
 				</div>

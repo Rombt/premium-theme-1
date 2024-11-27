93f75c0c70393ca0a8c9a5b40fb0e9fbacec7a78
Rombt <rombtnet@gmail.com>
2024-11-27 13:36:14 +0200

    .

--- app/src/front-page.php
+++ app/src/front-page.php
@@ -4,7 +4,7 @@
 
 <main>
 	<?php
-	get_template_part( 'pages\_templates\sections\hero_block_3\hero_block_3', null );
+	get_template_part( 'pages\parts\hero_block_1\hero_block_1', null );
 	get_template_part( 'pages\_templates\sections\section_cards-0\section_cards-0', '0' );
 	?>
 </main>
--- app/src/pages/parts/header-2/header-2.php
+++ app/src/pages/parts/header-2/header-2.php
@@ -1,35 +1,38 @@
-<div class="rmbt-full-width rmbt-header-wireframes-2-full-width">
+<div class="rmbt-full-width rmbt-header-2-full-width">
 
-   <section class="rmbt-container rmbt-header-wireframes-2">
+	<section class="rmbt-container rmbt-header-2">
 
-      <div class="rmbt-header-wireframes-2__row rmbt-header-wireframes-2-top-row">
+		<div class="rmbt-header-2__row rmbt-header-2-top-row">
 
-         <div class="rmbt-header-wireframes-2__col rmbt-header-wireframes-2-top-col-left">
-            <div class="block-details">
-               phones and emails
-            </div>
-         </div>
-         <div class="rmbt-header-wireframes-2__col rmbt-header-wireframes-2-top-col-right">
-            <div class="block-details"> icons of search, languages, cart, login
-               <p class='description'>search icon после 768 нужно убрать </p>
-            </div>
-         </div>
-      </div>
-      <div class="rmbt-header-wireframes-2__row rmbt-header-wireframes-2-bottom-row">
+			<div class="rmbt-header-2__col rmbt-header-2-top-col-left">
+				<div class="block-details">
+					phones
+				</div>
+				<div class="block-details">
+					emails
+				</div>
+			</div>
+			<div class="rmbt-header-2__col rmbt-header-2-top-col-right">
+				<div class="block-details"> icons of search, languages, cart, login
+					<p class='description'>search icon после 768 нужно убрать </p>
+				</div>
+			</div>
+		</div>
+		<div class="rmbt-header-2__row rmbt-header-2-bottom-row">
 
-         <div class="rmbt-header-wireframes-2__col rmbt-header-wireframes-2-bottom-col-left">
-            <div class="block-details"> logo</div>
-         </div>
-         <div class="rmbt-header-wireframes-2__col rmbt-header-wireframes-2-bottom-col-center">
-            <div class="block-details block-details-main-header-menu">main header menu</div>
-            <div class="block-details block-details-search">search field</div>
-         </div>
-         <div class="rmbt-header-wireframes-2__col rmbt-header-wireframes-2-bottom-col-burger">
-            <div class="block-details">burger</div>
-         </div>
-         <div class="rmbt-header-wireframes-2__col rmbt-header-wireframes-2-bottom-col-right">
-            <div class="block-details">call-to-action button</div>
-         </div>
-      </div>
-   </section>
+			<div class="rmbt-header-2__col rmbt-header-2-bottom-col-left">
+				<div class="block-details"> logo</div>
+			</div>
+			<div class="rmbt-header-2__col rmbt-header-2-bottom-col-center">
+				<div class="block-details block-details-main-header-menu">main header menu</div>
+				<div class="block-details block-details-search">search field</div>
+			</div>
+			<div class="rmbt-header-2__col rmbt-header-2-bottom-col-burger">
+				<div class="block-details">burger</div>
+			</div>
+			<div class="rmbt-header-2__col rmbt-header-2-bottom-col-right">
+				<div class="block-details">call-to-action button</div>
+			</div>
+		</div>
+	</section>
 </div>

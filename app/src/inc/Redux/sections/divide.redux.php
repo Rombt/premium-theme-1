be9513a1232800f69c8216c0b8d3463bd1fd4c8e
Rombt <rombtnet@gmail.com>
2024-11-28 11:07:38 +0200

    .

---
+++ app/src/inc/Redux/sections/divide.redux.php
@@ -0,0 +1,11 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+
+Redux::set_section(
+	$opt_name,
+	array(
+		'id' => 'divider_1',
+		'type' => 'divide',
+	)
+);
--- app/src/pages/parts/header-2/header-2.redux.php
+++ app/src/pages/parts/header-2/header-2.redux.php
@@ -2,89 +2,89 @@
 defined( 'ABSPATH' ) || exit;
 
 
-Redux::set_section(
-	$opt_name,
-	array(
-		'title' => esc_html__( 'header_wireframes-1', RMBT_TEXT_DOMAIN_THEME ),
-		'id' => 'settings_header_wireframes-1',
-		'desc' => esc_html__( 'Settings header site', RMBT_TEXT_DOMAIN_THEME ),
-		'customizer_width' => '450',
-		'subsections' => true,
-		// 'icon'             => 'el el-home',
-		'fields' => array(
+// Redux::set_section(
+// 	$opt_name,
+// 	array(
+// 		'title' => esc_html__( 'header_wireframes-1', RMBT_TEXT_DOMAIN_THEME ),
+// 		'id' => 'settings_header_wireframes-1',
+// 		'desc' => esc_html__( 'Settings header site', RMBT_TEXT_DOMAIN_THEME ),
+// 		'customizer_width' => '450',
+// 		'subsections' => true,
+// 		// 'icon'             => 'el el-home',
+// 		'fields' => array(
 
-			array(
-				'id' => 'rmbt-bg_section-img',
-				'type' => 'media',
-				'url' => true,
-				'title' => esc_html__( 'This picture will use for background section', RMBT_TEXT_DOMAIN_THEME ),
-				'compiler' => 'true',
-				'preview_size' => 'full',
-				'default' => array(
-					'url' => '/assets/img/no-image.jpg',
-				),
-			),
-			array(
-				'id' => 'rmbt-bg_section-img_alt',
-				'type' => 'text',
-				'title' => esc_html__( 'Description for background section picture', RMBT_TEXT_DOMAIN_THEME ),
-				'default' => esc_html__( 'background section picture', RMBT_TEXT_DOMAIN_THEME ),
-			),
+// 			array(
+// 				'id' => 'rmbt-bg_section-img',
+// 				'type' => 'media',
+// 				'url' => true,
+// 				'title' => esc_html__( 'This picture will use for background section', RMBT_TEXT_DOMAIN_THEME ),
+// 				'compiler' => 'true',
+// 				'preview_size' => 'full',
+// 				'default' => array(
+// 					'url' => '/assets/img/no-image.jpg',
+// 				),
+// 			),
+// 			array(
+// 				'id' => 'rmbt-bg_section-img_alt',
+// 				'type' => 'text',
+// 				'title' => esc_html__( 'Description for background section picture', RMBT_TEXT_DOMAIN_THEME ),
+// 				'default' => esc_html__( 'background section picture', RMBT_TEXT_DOMAIN_THEME ),
+// 			),
 
-			array(
-				'id' => 'rmbt-header_wireframes-1_section-title',
-				'type' => 'text',
-				'title' => esc_html__( 'Title of header_wireframes-1', RMBT_TEXT_DOMAIN_THEME ),
-				// 'default' => esc_html__( '', RMBT_TEXT_DOMAIN_THEME ),
-			),
-			array(
-				'id' => 'rmbt-header_wireframes-1_section-text',
-				'type' => 'text',
-				'title' => esc_html__( 'Text of header_wireframes-1', RMBT_TEXT_DOMAIN_THEME ),
-				// 'default' => esc_html__( '', RMBT_TEXT_DOMAIN_THEME ),
-			),
-
-
-
-			/*------------------ rmbt-header_wireframes-1 accordion ------------------*/
-			array(
-				'id' => 'rmbt-header_wireframes-1-start',
-				'type' => 'accordion',
-				'title' => esc_html__( 'Title of header_wireframes-1', RMBT_TEXT_DOMAIN_THEME ),
-				'subtitle' => 'Add your content to the section \'Title\'',
-				'position' => 'start',
-			),
+// 			array(
+// 				'id' => 'rmbt-header_wireframes-1_section-title',
+// 				'type' => 'text',
+// 				'title' => esc_html__( 'Title of header_wireframes-1', RMBT_TEXT_DOMAIN_THEME ),
+// 				// 'default' => esc_html__( '', RMBT_TEXT_DOMAIN_THEME ),
+// 			),
+// 			array(
+// 				'id' => 'rmbt-header_wireframes-1_section-text',
+// 				'type' => 'text',
+// 				'title' => esc_html__( 'Text of header_wireframes-1', RMBT_TEXT_DOMAIN_THEME ),
+// 				// 'default' => esc_html__( '', RMBT_TEXT_DOMAIN_THEME ),
+// 			),
 
 
-			// array(
-			// 	'id' => 'rmbt-header_wireframes-1-gallery',
-			// 	'type' => 'gallery',
-			// 	'title' => esc_html__( 'Add/Edit Gallery on the main screen ', RMBT_TEXT_DOMAIN_THEME ),
-			// ),
 
-			// array(
-			// 	'id' => 'header_wireframes-1_title',
-			// 	'type' => 'text',
-			// 	'title' => esc_html__( 'Front page title', RMBT_TEXT_DOMAIN_THEME ),
-			// 	'default' => __( wp_kses( 'Український виробник', 'post' ), RMBT_TEXT_DOMAIN_THEME ),
-			// ),
-
-			// array(
-			// 	'id' => 'header_wireframes-1_subtitle',
-			// 	'type' => 'text',
-			// 	'title' => esc_html__( 'Front page title', RMBT_TEXT_DOMAIN_THEME ),
-			// 	'default' => __( wp_kses( 'хлібопекарського і кондитерського обладнання', 'post' ), RMBT_TEXT_DOMAIN_THEME ),
-			// ),
+// 			/*------------------ rmbt-header_wireframes-1 accordion ------------------*/
+// 			array(
+// 				'id' => 'rmbt-header_wireframes-1-start',
+// 				'type' => 'accordion',
+// 				'title' => esc_html__( 'Title of header_wireframes-1', RMBT_TEXT_DOMAIN_THEME ),
+// 				'subtitle' => 'Add your content to the section \'Title\'',
+// 				'position' => 'start',
+// 			),
 
 
-			array(
-				'id' => 'rmbt-header_wireframes-1-end',
-				'type' => 'accordion',
-				'position' => 'end',
-			),
-			/*------------------ /rmbt-header_wireframes-1 accordion ------------------*/
+// 			// array(
+// 			// 	'id' => 'rmbt-header_wireframes-1-gallery',
+// 			// 	'type' => 'gallery',
+// 			// 	'title' => esc_html__( 'Add/Edit Gallery on the main screen ', RMBT_TEXT_DOMAIN_THEME ),
+// 			// ),
+
+// 			// array(
+// 			// 	'id' => 'header_wireframes-1_title',
+// 			// 	'type' => 'text',
+// 			// 	'title' => esc_html__( 'Front page title', RMBT_TEXT_DOMAIN_THEME ),
+// 			// 	'default' => __( wp_kses( 'Український виробник', 'post' ), RMBT_TEXT_DOMAIN_THEME ),
+// 			// ),
+
+// 			// array(
+// 			// 	'id' => 'header_wireframes-1_subtitle',
+// 			// 	'type' => 'text',
+// 			// 	'title' => esc_html__( 'Front page title', RMBT_TEXT_DOMAIN_THEME ),
+// 			// 	'default' => __( wp_kses( 'хлібопекарського і кондитерського обладнання', 'post' ), RMBT_TEXT_DOMAIN_THEME ),
+// 			// ),
 
 
-		),
-	)
-);
+// 			array(
+// 				'id' => 'rmbt-header_wireframes-1-end',
+// 				'type' => 'accordion',
+// 				'position' => 'end',
+// 			),
+// 			/*------------------ /rmbt-header_wireframes-1 accordion ------------------*/
+
+
+// 		),
+// 	)
+// );

43eda720b427f61785bd7071ed8417b657f374c1
Rombt <rombtnet@gmail.com>
2024-12-01 16:41:40 +0200

    .

--- app/src/pages/parts/header-2/header-2.php
+++ app/src/pages/parts/header-2/header-2.php
@@ -14,9 +14,34 @@
 				</div>
 			</div>
 			<div class="rmbt-header-2__col rmbt-header-2-top-col-right">
-				<div class="block-details"> icons of search, languages, cart, login
-					<p class='description'>search icon после 768 нужно убрать </p>
+				<a href="#rmbt-search_modal" class="rmbt-search-modal__trigger popup-link">
+					<svg>
+						<use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#search_5_2">
+						</use>
+					</svg>
+				</a>
+				<div id="rmbt-search_modal" class="rmbt-search-modal popup">
+					<div class="rmbt-search-modal__body popup__body">
+						<div class="rmbt-search-modal__close-window popup__close-window"></div>
+						<div class="rmbt-search-modal__content">
+							<h2 class="rmbt-search-modal__title popup__title section-title">
+								<?php esc_html_e( 'Пошук по сайту', 'rmbt_impex' ) ?>
+							</h2>
+							<div class="rmbt-search-modal__text popup__text">
+								<form class="rmbt-search-modal__form" role="search" method="get"
+									action="<?php echo esc_url( site_url() ); ?>">
+									<input type="search" value="<?php echo get_search_query(); ?>" name="s" id="s"
+										placeholder="<?php esc_html_e( 'Введіть текст...', 'rmbt_impex' ) ?>" />
+									<input type="submit" class="rmbt-impex-button"
+										value="<?php esc_html_e( 'пошук', 'rmbt_impex' ) ?>" />
+								</form>
+							</div>
+						</div>
+					</div>
 				</div>
+
+
+
 			</div>
 		</div>
 

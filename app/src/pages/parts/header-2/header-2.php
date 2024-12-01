7299369e4d4145f8ecd5ced66b738bb30fc3ab27
Rombt <rombtnet@gmail.com>
2024-12-01 17:09:25 +0200

    .

--- app/src/pages/parts/header-2/header-2.php
+++ app/src/pages/parts/header-2/header-2.php
@@ -1,51 +1,58 @@
 <div class="rmbt-full-width rmbt-header-2-top-row-full-width">
 
-	<section class="rmbt-container rmbt-header-2-top-row">
+   <section class="rmbt-container rmbt-header-2-top-row">
 
-		<div class="rmbt-header-2__row rmbt-header-2-top-row">
+      <div class="rmbt-header-2__row rmbt-header-2-top-row">
 
-			<div class="rmbt-header-2__col rmbt-header-2-top-col-left">
-				<div class="rmbt-header-2-top-col-left__phones">
-					<?php echo rmbt_redux_field_to_ul( 'rmbt-manager-1-phone' ); ?>
-					<?php echo rmbt_redux_field_to_ul( 'rmbt-manager-2-phone' ); ?>
-				</div>
-				<div class=rmbt-header-2-top-col-left__email">
-					<?php echo rmbt_redux_field_to_ul( 'rmbt-manager-1-email', 'mailto' ); ?>
-				</div>
-			</div>
-			<div class="rmbt-header-2__col rmbt-header-2-top-col-right">
-				<a href="#rmbt-search_modal" class="rmbt-search-modal__trigger popup-link">
-					<svg>
-						<use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#search_5_2">
-						</use>
-					</svg>
-				</a>
-				<div id="rmbt-search_modal" class="rmbt-search-modal popup">
-					<div class="rmbt-search-modal__body popup__body">
-						<div class="rmbt-search-modal__close-window popup__close-window"></div>
-						<div class="rmbt-search-modal__content">
-							<h2 class="rmbt-search-modal__title popup__title section-title">
-								<?php esc_html_e( 'Пошук по сайту', 'rmbt_impex' ) ?>
-							</h2>
-							<div class="rmbt-search-modal__text popup__text">
-								<form class="rmbt-search-modal__form" role="search" method="get"
-									action="<?php echo esc_url( site_url() ); ?>">
-									<input type="search" value="<?php echo get_search_query(); ?>" name="s" id="s"
-										placeholder="<?php esc_html_e( 'Введіть текст...', 'rmbt_impex' ) ?>" />
-									<input type="submit" class="rmbt-impex-button"
-										value="<?php esc_html_e( 'пошук', 'rmbt_impex' ) ?>" />
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
+         <div class="rmbt-header-2__col rmbt-header-2-top-col-left">
+            <div class="rmbt-header-2-top-col-left__phones">
+               <?php echo rmbt_redux_field_to_ul( 'rmbt-manager-1-phone' ); ?>
+               <?php echo rmbt_redux_field_to_ul( 'rmbt-manager-2-phone' ); ?>
+            </div>
+            <div class=rmbt-header-2-top-col-left__email">
+               <?php echo rmbt_redux_field_to_ul( 'rmbt-manager-1-email', 'mailto' ); ?>
+            </div>
+         </div>
+         <div class="rmbt-header-2__col rmbt-header-2-top-col-right">
+            <!-- icons of search, languages, cart, login -->
+            <a href="#rmbt-search_modal" class="rmbt-search-modal__trigger popup-link">
+               <svg>
+                  <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#search_5_2">
+                  </use>
+               </svg>
+            </a>
+            <a href="#" class="">
+               <svg>
+                  <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#cart_4_2">
+                  </use>
+               </svg>
+            </a>
+            <div id="rmbt-search_modal" class="rmbt-search-modal popup">
+               <div class="rmbt-search-modal__body popup__body">
+                  <div class="rmbt-search-modal__close-window popup__close-window"></div>
+                  <div class="rmbt-search-modal__content">
+                     <h2 class="rmbt-search-modal__title popup__title section-title">
+                        <?php esc_html_e( 'Пошук по сайту', 'rmbt_impex' ) ?>
+                     </h2>
+                     <div class="rmbt-search-modal__text popup__text">
+                        <form class="rmbt-search-modal__form" role="search" method="get"
+                           action="<?php echo esc_url( site_url() ); ?>">
+                           <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="s"
+                              placeholder="<?php esc_html_e( 'Введіть текст...', 'rmbt_impex' ) ?>" />
+                           <input type="submit" class="rmbt-impex-button"
+                              value="<?php esc_html_e( 'пошук', 'rmbt_impex' ) ?>" />
+                        </form>
+                     </div>
+                  </div>
+               </div>
+            </div>
 
 
 
-			</div>
-		</div>
+         </div>
+      </div>
 
-	</section>
+   </section>
 
 
 </div>

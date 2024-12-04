afa8f3a750885d8eaaa993bdbb88a8a0def6fda4
Rombt <rombtnet@gmail.com>
2024-12-04 10:44:13 +0200

    .

--- app/src/pages/parts/header-2/header-2.php
+++ app/src/pages/parts/header-2/header-2.php
@@ -5,22 +5,21 @@
 			<div class="rmbt-header-2__col rmbt-header-2-top-col-left">
 				<!-- <div class="rmbt-header-2-top-col-left__phones" data-da=".rmbt-header-2-top-col-center, 768"> -->
 
-				<div class="rmbt-header-2-top-col-left__email">
-					<?php echo rmbt_redux_field_to_ul( 'rmbt-manager-1-email', 'mailto' ); ?>
-				</div>
-				<svg>
+				<svg id='svg_email'>
 					<use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#email_1">
 					</use>
 				</svg>
-
+				<svg id='svg_phone'>
+					<use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#phone_1">
+					</use>
+				</svg>
+				<div class="rmbt-header-2-top-col-left__email">
+					<?php echo rmbt_redux_field_to_ul( 'rmbt-manager-1-email', 'mailto' ); ?>
+				</div>
 				<div class="rmbt-header-2-top-col-left__phones">
 					<?php echo rmbt_redux_field_to_ul( 'rmbt-manager-1-phone' ); ?>
 					<?php echo rmbt_redux_field_to_ul( 'rmbt-manager-2-phone' ); ?>
 
-					<!-- <svg>
-						<use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#phone_1">
-						</use>
-					</svg> -->
 				</div>
 
 				<!-- <div class=rmbt-header-2-top-col-left__email" data-da=".rmbt-header-2-top-col-left, 768"> -->

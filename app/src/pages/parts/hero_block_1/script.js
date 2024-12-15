b79cf0c8534dd8e5898a4554d58ad8f7f83c1f6f
Rombt <rombtnet@gmail.com>
2024-12-15 12:19:11 +0200

    +

--- app/src/pages/parts/hero_block_1/script.js
+++ app/src/pages/parts/hero_block_1/script.js
@@ -2,6 +2,7 @@
   const burgerMenu = document.querySelector('.rmbt-menu-horizontal .icon-burger');
   const topRowColLeft = document.querySelector('.rmbt-hero-block-1-top-row-col-left');
   const logo = topRowColLeft.querySelector('.custom-logo-link');
+  const heroBlockFullWidth = document.querySelector('.rmbt-hero-block-1-full-width');
   const menuHeaderNavigationContainer = document.querySelector(
     '.menu-header-navigation-container'
   );
@@ -12,6 +13,7 @@
 
   burgerMenu.addEventListener('click', e => {
     menuHeaderNavigationContainer.prepend(logo);
+    heroBlockFullWidth.classList.add('hide-after');
   });
 
   document.addEventListener('click', e => {
@@ -20,6 +22,7 @@
       !menuHeaderNavigationContainer.contains(e.target)
     ) {
       topRowColLeft.prepend(logo);
+      heroBlockFullWidth.classList.remove('hide-after');
     }
   });
 })();

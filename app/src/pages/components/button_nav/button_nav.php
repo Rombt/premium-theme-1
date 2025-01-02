f40b942be25bed4077854785e49a4fa83e037a3a
Rombt <rombtnet@gmail.com>
2025-01-02 14:24:46 +0200

    .

---
+++ app/src/pages/components/button_nav/button_nav.php
@@ -0,0 +1,3 @@
+<a href="<?php echo $args['href'] ?>" class="rmbt-button-nav" <?= ! empty( $args['data'] ) ? $args['data'] : '' ?>>
+	<span><?php echo $args['title'] ?></span>
+</a>
--- app/src/pages/components/controls_container/controls_container.php
+++ app/src/pages/components/controls_container/controls_container.php
@@ -1,6 +1,13 @@
 <div class="controls-container">
 
-	<?php get_template_part( 'pages/components/button_link/button_link', 'null', [ 
+	<?php get_template_part( 'pages/components/button_nav/button_nav', 'null', [ 
+		'data' => 'data-da=""',
+		'title' => rmbt_get_redux_field( 'rmbt-call_to_action_button-text', 1 ),
+		'href' => rmbt_get_redux_field( 'rmbt-call_to_action_button-link', 1 ),
+	] );
+	?>
+
+	<?php get_template_part( 'pages/components/button_nav/button_nav', 'null', [ 
 		'data' => 'data-da=""',
 		'title' => rmbt_get_redux_field( 'rmbt-call_to_action_button-text', 1 ),
 		'href' => rmbt_get_redux_field( 'rmbt-call_to_action_button-link', 1 ),

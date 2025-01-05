426df127ffaa91476208f293f02eb748870fcc31
Rombt <rombtnet@gmail.com>
2025-01-05 15:18:18 +0200

    benefit slider is ready

--- app/src/pages/components/benefit_slide/benefit_slide.php
+++ app/src/pages/components/benefit_slide/benefit_slide.php
@@ -1,15 +1,81 @@
-<div class="benefit-slide">
+<!-- <div class="benefit-slide">
 	<div class="benefit-slide__title">
-		<?php echo rmbt_get_redux_field( 'rmbt-benefits-slide-block_title_1', 1 ) ?>
+		<?php // echo rmbt_get_redux_field( 'rmbt-benefits-slide-block_title_1', 1 ) ?>
 	</div>
 
 	<div class="benefit-slide__text">
-		<?php echo rmbt_get_redux_field( 'rmbt-benefits-slide-block_text_1', 1 ) ?>
+		<?php // echo rmbt_get_redux_field( 'rmbt-benefits-slide-block_text_1', 1 ) ?>
 	</div>
-	<?php get_template_part( 'pages/components/button_nav/button_nav', 'null', [ 
-		'data' => 'data-da=""',
-		'title' => rmbt_get_redux_field( 'rmbt-benefits-slide-block_button-title_1', 1 ),
-		'href' => rmbt_get_redux_field( 'rmbt-benefits-slide-block_button-link_1', 1 ),
-	] );
-	?>
+	 <?php // get_template_part( 'pages/components/button_nav/button_nav', 'null', [  -->
+// 'data' => 'data-da=""',
+// 'title' => rmbt_get_redux_field( 'rmbt-benefits-slide-block_button-title_1', 1 ),
+// 'href' => rmbt_get_redux_field( 'rmbt-benefits-slide-block_button-link_1', 1 ),
+// ] );
+	 ?>
+</div> -->
+
+
+<div class="rmbt-benefit-slide-swiper swiper">
+	<div class="swiper-wrapper">
+		<div class="swiper-slide benefit-slide">
+			<div class="benefit-slide__title">
+				<?php echo rmbt_get_redux_field( 'rmbt-benefits-slide-block_title_1', 1 ) ?>
+			</div>
+
+			<div class="benefit-slide__text">
+				<?php echo rmbt_get_redux_field( 'rmbt-benefits-slide-block_text_1', 1 ) ?>
+			</div>
+			<?php get_template_part( 'pages/components/button_nav/button_nav', 'null', [ 
+				'data' => 'data-da=""',
+				'title' => rmbt_get_redux_field( 'rmbt-benefits-slide-block_button-title_1', 1 ),
+				'href' => rmbt_get_redux_field( 'rmbt-benefits-slide-block_button-link_1', 1 ),
+			] );
+			?>
+		</div>
+		<div class="swiper-slide benefit-slide">
+			<div class="benefit-slide__title">
+				<?php echo rmbt_get_redux_field( 'rmbt-benefits-slide-block_title_2', 1 ) ?>
+			</div>
+
+			<div class="benefit-slide__text">
+				<?php echo rmbt_get_redux_field( 'rmbt-benefits-slide-block_text_2', 1 ) ?>
+			</div>
+			<?php get_template_part( 'pages/components/button_nav/button_nav', 'null', [ 
+				'data' => 'data-da=""',
+				'title' => rmbt_get_redux_field( 'rmbt-benefits-slide-block_button-title_2', 1 ),
+				'href' => rmbt_get_redux_field( 'rmbt-benefits-slide-block_button-link_2', 1 ),
+			] );
+			?>
+		</div>
+		<div class="swiper-slide benefit-slide">
+			<div class="benefit-slide__title">
+				<?php echo rmbt_get_redux_field( 'rmbt-benefits-slide-block_title_3', 1 ) ?>
+			</div>
+
+			<div class="benefit-slide__text">
+				<?php echo rmbt_get_redux_field( 'rmbt-benefits-slide-block_text_3', 1 ) ?>
+			</div>
+			<?php get_template_part( 'pages/components/button_nav/button_nav', 'null', [ 
+				'data' => 'data-da=""',
+				'title' => rmbt_get_redux_field( 'rmbt-benefits-slide-block_button-title_3', 1 ),
+				'href' => rmbt_get_redux_field( 'rmbt-benefits-slide-block_button-link_3', 1 ),
+			] );
+			?>
+		</div>
+		<div class="swiper-slide benefit-slide">
+			<div class="benefit-slide__title">
+				<?php echo rmbt_get_redux_field( 'rmbt-benefits-slide-block_title_4', 1 ) ?>
+			</div>
+
+			<div class="benefit-slide__text">
+				<?php echo rmbt_get_redux_field( 'rmbt-benefits-slide-block_text_4', 1 ) ?>
+			</div>
+			<?php get_template_part( 'pages/components/button_nav/button_nav', 'null', [ 
+				'data' => 'data-da=""',
+				'title' => rmbt_get_redux_field( 'rmbt-benefits-slide-block_button-title_4', 1 ),
+				'href' => rmbt_get_redux_field( 'rmbt-benefits-slide-block_button-link_4', 1 ),
+			] );
+			?>
+		</div>
+	</div>
 </div>

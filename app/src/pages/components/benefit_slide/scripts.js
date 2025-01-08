9ea7fc34729fc29e580f6d06efa70fe61f96e906
Rombt <rombtnet@gmail.com>
2025-01-08 20:22:05 +0200

    .

---
+++ app/src/pages/components/benefit_slide/scripts.js
@@ -0,0 +1,10 @@
+if (document.querySelector('.rmbt-benefit-slide-swiper')) {
+  swiperParam = new Swiper('.rmbt-benefit-slide-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    speed: 700,
+    autoplay: {
+      delay: 50000,
+    },
+  });
+}

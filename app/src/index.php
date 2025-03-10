faa77c20c77e08458438536beecaf07af4da23ed
Rombt <rombtnet@gmail.com>
2025-03-10 20:55:33 +0200

    .

--- app/src/index.php
+++ app/src/index.php
@@ -1,11 +1,24 @@
 <?php get_header(); ?>
 
 
-<div class="rmbt-container">
-	<main>
-		<?php get_template_part( 'pages/parts/loop/loop' ); ?>
-	</main>
-	<?php get_sidebar(); ?>
-</div>
+
+<main>
+
+	<div class="rmbt-full-width rmbt-blog-full-width">
+		<section class="rmbt-container rmbt-blog" <?php if ( isset( $rmbt_theme_options['rmbt-blog_section-title'] ) && $rmbt_theme_options['rmbt-blog_section-title'] == "" ) :
+			echo 'style="padding-top: 15px; padding-bottom: 15px;"';
+		endif ?>>
+			<div class="rmbt-blog__row">
+				<div class="rmbt-blog__col rmbt-blog-right-col">
+					<?php get_template_part( 'pages/parts/loop/loop' ); ?>
+				</div>
+				<?php get_sidebar(); ?>
+			</div>
+		</section>
+	</div>
+
+
+</main>
+<!-- </div> -->
 
 <?php get_footer(); ?>
--- app/src/pages/parts/loop/loop.php
+++ app/src/pages/parts/loop/loop.php
@@ -1,31 +1,12 @@
-<div class="rmbt-full-width rmbt-blog-full-width">
-	<section class="rmbt-container rmbt-blog" <?php if ( isset( $rmbt_theme_options['rmbt-blog_section-title'] ) && $rmbt_theme_options['rmbt-blog_section-title'] == "" ) :
-		echo 'style="padding-top: 15px; padding-bottom: 15px;"';
-	endif ?>>
+<?php if ( have_posts() ) : ?>
+	<?php while ( have_posts() ) :
+		the_post(); ?>
+		<?php get_template_part( 'pages/parts/content/content', get_post_format() ); ?>
+	<?php endwhile; ?>
 
-
-		<div class="rmbt-blog__row">
-
-
-
-			<div class="rmbt-blog__col rmbt-blog-right-col">
-
-				<?php if ( have_posts() ) : ?>
-					<?php while ( have_posts() ) :
-						the_post(); ?>
-						<?php get_template_part( 'pages/parts/content/content', get_post_format() ); ?>
-					<?php endwhile; ?>
-
-					<div class="pagination">
-						<?php echo paginate_links(); ?>
-					</div>
-				<?php else : ?>
-					<p>Posts don't found</p>
-				<?php endif; ?>
-
-			</div>
-		</div>
-
-
-	</section>
-</div>
+	<div class="pagination">
+		<?php echo paginate_links(); ?>
+	</div>
+<?php else : ?>
+	<p>Posts don't found</p>
+<?php endif; ?>

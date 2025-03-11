500dd064975c00fb9324804fc886e001b718f65a
Rombt <rombtnet@gmail.com>
2025-03-11 22:05:06 +0200

    .

--- app/src/sidebar.php
+++ app/src/sidebar.php
@@ -1,6 +1,6 @@
 <aside class="sidebar">
-	<?php if ( is_active_sidebar( 'main-sidebar' ) ) : ?>
-		<?php dynamic_sidebar( 'main-sidebar' ); ?>
+	<?php if ( is_active_sidebar( 'rmbt_blog_sidebar' ) ) : ?>
+		<?php dynamic_sidebar( 'rmbt_blog_sidebar' ); ?>
 	<?php else : ?>
 		<p>Добавьте виджеты в админке.</p>
 	<?php endif; ?>

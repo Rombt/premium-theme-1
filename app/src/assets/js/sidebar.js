cc8baed5601b85ad56d2495e0c2797fb4ccc99fe
Rombt <rombtnet@gmail.com>
2025-03-15 15:28:02 +0200

    ,

--- app/src/assets/js/sidebar.js
+++ app/src/assets/js/sidebar.js
@@ -1,6 +1,9 @@
 function sidebarToggle() {
   const sidebar = document.querySelector('.sidebar');
   const sidebarToggle = document.querySelector('.sidebar-toggle');
+  const parentSidebarToggle = sidebarToggle.parentElement;
+
+  console.log('parentSidebarToggle = ', parentSidebarToggle);
 
   if (!sidebar || !sidebarToggle) {
     return;
@@ -8,6 +11,7 @@ function sidebarToggle() {
 
   sidebarToggle.addEventListener('click', () => {
     sidebar.classList.toggle('open');
+    sidebar.prepend(sidebarToggle);
   });
 
   document.addEventListener('click', e => {
@@ -17,6 +21,7 @@ function sidebarToggle() {
       !sidebarToggle.contains(e.target)
     ) {
       sidebar.classList.remove('open');
+      parentSidebarToggle.append(sidebarToggle);
     }
   });
 }

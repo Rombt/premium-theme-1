5571af592cac23f78ba1536dccebfafbf63bb17b
Rombt <rombtnet@gmail.com>
2025-03-15 17:02:34 +0200

    .

--- app/src/sidebar.php
+++ app/src/sidebar.php
@@ -1,4 +1,5 @@
 <aside class="sidebar">
+	<button class="sidebar-toggle" data-da=".rmbt-hero-block-1-top-row__row, 1024, last">☰</button>
 	<?php if ( is_active_sidebar( 'rmbt_blog_sidebar' ) ) : ?>
 		<?php dynamic_sidebar( 'rmbt_blog_sidebar' ); ?>
 	<?php else : ?>

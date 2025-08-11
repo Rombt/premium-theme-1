345e36e3ee362bc8c64ff4e6ff9918192e2b32dc
Rombt <rombtnet@gmail.com>
2025-08-11 19:50:45 +0300

    .

--- app/src/pages/parts/header-2/header-2.php
+++ app/src/pages/parts/header-2/header-2.php
@@ -5,14 +5,13 @@
 			<div class="rmbt-header-2__col rmbt-header-2-top-col-left">
 				<div class="rmbt-header-2-top-col-left__email">
 					<?php echo rmbt_redux_field_to_ul('rmbt-manager-1-email', 'mailto'); ?>
-					<div class="ul-toggle-wrap">					<svg>
-						<use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#triangle">
-							</use>
+					<div class="ul-toggle-wrap">					
+						<svg>
+						<use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#triangle"> </use>
 						</svg>	
 					</div>
 					<svg>
-						<use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#email_1">
-						</use>
+						<use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#email_1"> </use>
 					</svg>	
 				</div>
 				<div class="rmbt-header-2-top-col-left__phones">

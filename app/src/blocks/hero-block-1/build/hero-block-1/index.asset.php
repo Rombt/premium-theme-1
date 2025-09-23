0e612fc93037ed02689b8d0faa59aa94a51331f8
Rombt <rombtnet@gmail.com>
2025-09-23 11:40:40 +0300

    .

--- app/src/blocks/hero-block-1/build/hero-block-1/index.asset.php
+++ app/src/blocks/hero-block-1/build/hero-block-1/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f0c07cb3dc20f23ee73c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '46d7eba18bbca4947ab2');
--- app/src/blocks/hero-block-1/build/hero-block-1/view.asset.php
+++ app/src/blocks/hero-block-1/build/hero-block-1/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'd88e4e6de4d749a4989a');
+<?php return array('dependencies' => array(), 'version' => 'a7bf1b362f012c48fd77');

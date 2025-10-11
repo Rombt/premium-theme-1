21385b05e627304d882e78076f94fa33c08b895b
Rombt <rombtnet@gmail.com>
2025-10-11 14:33:01 +0300

    .

--- app/src/pages/parts/featured_projects/script.js
+++ app/src/pages/parts/featured_projects/script.js
@@ -3,7 +3,7 @@ if (document.querySelector('.rmbt-featured-projects-slide-swiper')) {
     direction: 'horizontal',
     loop: true,
     spaceBetween: 30,
-    slidesPerView: 6,
+    slidesPerView: 4,
     centeredSlides: true,
     speed: 820,
 
@@ -12,6 +12,14 @@ if (document.querySelector('.rmbt-featured-projects-slide-swiper')) {
     // effect: 'cards',
     // effect: 'slide',
 
+    coverflowEffect: {
+      rotate: 2,       // градусы поворота
+      stretch: 30,       // отступ между слайдами
+      depth: 2,       // глубина по оси Z
+      modifier: 1.2,      // множитель эффекта
+      scale: 0.85,         // масштаб слайда
+      slideShadows: true // тени
+    },
 
     autoplay: {
       delay: 4800,

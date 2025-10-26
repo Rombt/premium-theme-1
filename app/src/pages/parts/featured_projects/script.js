0dab846eabcf09bbf45bf589bf800730079da176
Rombt <rombtnet@gmail.com>
2025-10-26 15:40:53 +0200

    front-page.php is ready

--- app/src/pages/parts/featured_projects/script.js
+++ app/src/pages/parts/featured_projects/script.js
@@ -3,14 +3,14 @@ if (document.querySelector('.rmbt-featured-projects-slide-swiper')) {
     direction: 'horizontal',
     loop: true,
     speed: 820,
-    slidesPerView: 'auto',
-    centeredSlides: true,
-
+    slidesPerView: 1,
+    centerInsufficientSlides: true,
+    
     effect: 'coverflow',
     coverflowEffect: {
-      rotate: 7,       
-      stretch: 30,       
-      depth: 3,       
+      rotate: 9,       
+      stretch: 20,       
+      depth: 7,       
       modifier: 1.5,      
       scale: 0.85,         
       slideShadows: true 
@@ -19,10 +19,19 @@ if (document.querySelector('.rmbt-featured-projects-slide-swiper')) {
     autoplay: {
       delay: 4800,
     },
+    
     navigation: {
       nextEl: '.rmbt-featured-projects-slide-swiper__button-next',
       prevEl: '.rmbt-featured-projects-slide-swiper__button-prev',
     },
+
+    breakpoints: {
+      650: {
+        slidesPerView: 'auto',
+        centerInsufficientSlides: true,
+      },
+    }
+
 
   });
 }

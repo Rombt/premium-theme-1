c74ae2b38fdfa4f9c7dc405925d4f4c8e0eb1c0f
Rombt <rombtnet@gmail.com>
2025-12-12 10:50:31 +0200

    исправил woocommerce\wc-functions.php

--- app/src/woocommerce/wc-functions.php
+++ app/src/woocommerce/wc-functions.php
@@ -1,9 +1,19 @@
 <?php
+/**
+ * WooCommerce Theme Functions.
+ *
+ * @package rmbt
+ */
 
 if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) {
 	exit;
 }
 
+/**
+ * Setup WooCommerce features for the theme.
+ *
+ * @return void
+ */
 function rmbt_wc_site_setup() {
 	add_theme_support( 'woocommerce' );
 }

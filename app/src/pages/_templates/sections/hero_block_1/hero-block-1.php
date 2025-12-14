8db33fa57b047f4e5232a4d0bade1a3472d5d4ac
Rombt <rombtnet@gmail.com>
2025-12-14 11:55:22 +0200

    hero-block-1.php

--- app/src/pages/_templates/sections/hero_block_1/hero-block-1.php
+++ app/src/pages/_templates/sections/hero_block_1/hero-block-1.php
@@ -1,31 +1,25 @@
+<?php
+/**
+ * Hero Block 1 template.
+ *
+ * @package Premium_Theme_1
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
 <div class="rmbt-full-width _rmbt-hero-block-1-full-width">
 	<section class="rmbt-container _rmbt-hero-block-1">
-
 		<div class="_rmbt-hero-block-1__row">
 			<div class='_rmbt-hero-block-1__col _rmbt-hero-block-1-col-right'>
-
 				<div class="_rmbt-hero-block-1-col-right__bg">
-					<!-- <div class="wrap-img">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/bg_transparent.jpg"
-							alt="hero-block-bg">
-					</div> -->
 				</div>
-
-
 			</div>
 			<div class='_rmbt-hero-block-1__col _rmbt-hero-block-1-col-left'>
-
 				<div class="_rmbt-hero-block-1-col-left__bg">
-					<!-- <div class="wrap-img">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/bg_transparent.jpg"
-							alt="hero-block-bg">
-					</div> -->
 				</div>
-
 			</div>
 			<div class="_rmbt-hero-block-1__title-block _rmbt-title-block">
-
-				<!-- <div class="_rmbt-title-block__decor"></div> -->
 				<div class="_rmbt-title-block__icon"></div>
 				<div class="_rmbt-title-block__subtitle">
 					<div class="block-details">
@@ -37,14 +31,7 @@
 						title
 					</div>
 				</div>
-				<!-- <a href="#" class='_rmbt-title-block__button'>
-					<div class="block-details">
-						cal-to-action-bottom
-					</div>
-				</a> -->
 				<?php get_template_part( 'template-parts/_templates/components\button-0\button-0' ); ?>
-
-
 			</div>
 			<div class="_rmbt-hero-block-1__right-text">
 				<div class="block-details">

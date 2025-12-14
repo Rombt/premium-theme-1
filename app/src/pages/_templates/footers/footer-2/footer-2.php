f5600e8c9b749f059d3739f4b87e832482f3e6ba
Rombt <rombtnet@gmail.com>
2025-12-14 13:53:17 +0200

    footer-2.php

--- app/src/pages/_templates/footers/footer-2/footer-2.php
+++ app/src/pages/_templates/footers/footer-2/footer-2.php
@@ -1,3 +1,13 @@
+<?php
+/**
+ * Footer 2 template.
+ *
+ * @package Premium_Theme_1
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
 <div class="rmbt-full-width rmbt-footer-2-full-width">
 	<section class="rmbt-container rmbt-footer-2">
 		<div class="rmbt-footer-2__row rmbt-footer-2-row-content">
@@ -11,7 +21,6 @@
 			</div>
 			<div class="rmbt-footer-2__col rmbt-footer-2-right-col">
 				<?php get_template_part( 'pages\_templates\blocks\social_media_icon_block-0\social_media_icon_block-0', '0' ); ?>
-
 			</div>
 		</div>
 

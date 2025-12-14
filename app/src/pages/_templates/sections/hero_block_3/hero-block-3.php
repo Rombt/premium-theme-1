701e477d2a5350c2353ae9869521104f851f5085
Rombt <rombtnet@gmail.com>
2025-12-14 11:38:26 +0200

    hero-block-3.php

--- app/src/pages/_templates/sections/hero_block_3/hero-block-3.php
+++ app/src/pages/_templates/sections/hero_block_3/hero-block-3.php
@@ -1,18 +1,22 @@
+<?php
+/**
+ * Hero Block 3 template.
+ *
+ * @package Premium_Theme_1
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
 <div class="rmbt-full-width rmbt-hero-block-3-full-width">
 	<section class="rmbt-container rmbt-hero-block-3">
-
 		<div class="rmbt-hero-block-3__row">
-
 			<div class='rmbt-hero-block-3__col rmbt-hero-block-3-col-left'>
 				<?php get_template_part( 'pages\_templates\components\rmbt_vertical_menu_0\rmbt_vertical_menu_0', null ); ?>
-
 			</div>
-
 			<div class='rmbt-hero-block-3__col rmbt-hero-block-3-col-center'>
 				<div class="rmbt-hero-block-3__bg">
 					<div class="wrap-img">
-						<!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bg_transparent.jpg"
-							alt="hero-block-bg"> -->
 					</div>
 				</div>
 				<div class="rmbt-hero-block-3-text">
@@ -28,33 +32,18 @@
 						</div>
 					</div>
 					<?php get_template_part( 'pages\_templates\components\button_0\button_0', null ); ?>
-
 				</div>
 			</div>
 			<div class='rmbt-hero-block-3__col rmbt-hero-block-3-col-right'>
-
 				<div class="rmbt-hero-block-3-col-right__top">
 					<div class="rmbt-hero-block-3-col-right__bg">
-						<!-- <div class="wrap-img">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/bg_transparent.jpg"
-							alt="hero-block-bg">
-					</div> -->
 					</div>
 					<?php get_template_part( 'template-parts/_templates/components\rmbt_vertical_menu_0\rmbt_vertical_menu_0', null, array( 'additional_class_menu' => 'right-top-menu' ) ); ?>
-
-
 				</div>
 				<div class="rmbt-hero-block-3-col-right__bottom">
 					<div class="rmbt-hero-block-3-col-right__bg">
-						<!-- <div class="wrap-img">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/bg_transparent.jpg"
-							alt="hero-block-bg">
-					</div> -->
 					</div>
 				</div>
-
-
-
 			</div>
 		</div>
 	</section>

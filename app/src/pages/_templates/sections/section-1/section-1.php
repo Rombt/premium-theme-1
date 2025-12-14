2eed9e9c6419faa177d2b727af38c914cd28717b
Rombt <rombtnet@gmail.com>
2025-12-14 11:27:37 +0200

    section-1.php

--- app/src/pages/_templates/sections/section-1/section-1.php
+++ app/src/pages/_templates/sections/section-1/section-1.php
@@ -1,45 +1,29 @@
+<?php
+/**
+ * Section 1 template.
+ *
+ * @package Premium_Theme_1
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
 <div class="rmbt-full-width rmbt-section-1-full-width">
 	<section class="rmbt-container rmbt-section-1">
-
 		<div class="rmbt-section-1__row">
 			<div class="rmbt-section-1__col rmbt-section-1-video">
-				<!-- <div class="block-details"> video </div> -->
-
 			</div>
 			<div class="rmbt-section-1__col rmbt-section-1-text">
 				<h2 class='rmbt-section-1-text__title'>
 					title
-					<?php // echo rmbt_get_redux_field( 'rmbt-section-1_section-title' ) ?>
-					<?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt-_section-title')]); ?>
 				</h2>
 				<p class='rmbt-section-1-text__subtitle'>
 					subtitle
-					<?php // echo rmbt_get_redux_field( 'rmbt-section-1_section-text' ) ?>
 				</p>
 				<p class='rmbt-section-1-text__text'>
 					text
-					<?php // echo rmbt_get_redux_field( 'rmbt-section-1_section-text' ) ?>
 				</p>
-
 			</div>
 		</div>
-
-
 	</section>
 </div>
-
-
-<!-- <h2 class='rmbt-section-1-head__title'>
-						<div class="block-details"> title </div>
-						<?php // echo rmbt_get_redux_field( 'rmbt-section-1_section-title' ) ?>
-						<?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt-_section-title')]); ?>
-					</h2>
-					<p class='rmbt-section-1-head__subtitle block-details'>
-						subtitle
-						<?php // echo rmbt_get_redux_field( 'rmbt-section-1_section-text' ) ?>
-					</p> -->
-
-
-<!-- <div class="rmbt-section-1__row rmbt-section-1-body">
-			<div class="block-details"> other content</div>
-		</div> -->

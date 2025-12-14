b66ca0aa22115e183f6b4b66ba28a0b3cc6a5e95
Rombt <rombtnet@gmail.com>
2025-12-14 11:22:30 +0200

    section-2.php

--- app/src/pages/_templates/sections/section-2/section-2.php
+++ app/src/pages/_templates/sections/section-2/section-2.php
@@ -1,3 +1,13 @@
+<?php
+/**
+ * Section 2 template.
+ *
+ * @package Premium_Theme_1
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
 <div class="rmbt-full-width rmbt-section-2-full-width">
 	<section class="rmbt-container rmbt-section-2">
 
@@ -6,12 +16,9 @@
 			<div class="rmbt-section-2-head">
 				<h2 class='rmbt-section-2-head__title'>
 					<div class="block-details"> title </div>
-					<?php // echo rmbt_get_redux_field( 'rmbt-section-2_section-title' ) ?>
-					<?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt-_section-title')]); ?>
 				</h2>
 				<p class='rmbt-section-2-head__subtitle block-details'>
 					subtitle
-					<?php // echo rmbt_get_redux_field( 'rmbt-section-2_section-text' ) ?>
 				</p>
 			</div>
 
@@ -23,7 +30,6 @@
 					<div class="block-details">
 					img
 					</div>
-					<!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/img/" alt=""> -->
 				</div>
 
 			</div>

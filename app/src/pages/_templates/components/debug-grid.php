b821e4b9a90d0375ddfa185aab7e9579a5c89255
Rombt <rombtnet@gmail.com>
2025-12-14 14:04:01 +0200

    debug-grid.php

--- app/src/pages/_templates/components/debug-grid.php
+++ app/src/pages/_templates/components/debug-grid.php
@@ -1,18 +1,28 @@
+<?php
+/**
+ * Debug Grid template (for development purposes).
+ *
+ * @package Premium_Theme_1
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
 <div class="debug-grid">
-   <div>
-      <div>
-         <div></div>
-         <div></div>
-         <div></div>
-         <div></div>
-         <div></div>
-         <div></div>
-         <div></div>
-         <div></div>
-         <div></div>
-         <div></div>
-         <div></div>
-         <div></div>
-      </div>
-   </div>
+	<div>
+		<div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		</div>
+	</div>
 </div>

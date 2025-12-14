6d199380c43faa0f0d20edffadb3a9babb8715d3
Rombt <rombtnet@gmail.com>
2025-12-14 12:05:25 +0200

    header-3.php

--- app/src/pages/_templates/headers/header-3/header-3.php
+++ app/src/pages/_templates/headers/header-3/header-3.php
@@ -1,35 +1,45 @@
+<?php
+/**
+ * Header 3 template.
+ *
+ * @package Premium_Theme_1
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
 <div class="rmbt-full-width rmbt-header-wireframes-3-full-width">
 
-   <section class="rmbt-container rmbt-header-wireframes-3">
+	<section class="rmbt-container rmbt-header-wireframes-3">
 
-      <div class="rmbt-header-wireframes-3__row rmbt-header-wireframes-3-top-row">
+		<div class="rmbt-header-wireframes-3__row rmbt-header-wireframes-3-top-row">
 
-         <div class="rmbt-header-wireframes-3__col rmbt-header-wireframes-3-top-col-left">
-            <div class="block-details">main header menu</div>
-         </div>
-         <div class="rmbt-header-wireframes-3__col rmbt-header-wireframes-3-top-col-right">
-            <div class="block-details"> languages, currencies</div>
-         </div>
-      </div>
-      <div class="rmbt-header-wireframes-3__row rmbt-header-wireframes-3-middle-row">
+		<div class="rmbt-header-wireframes-3__col rmbt-header-wireframes-3-top-col-left">
+			<div class="block-details">main header menu</div>
+		</div>
+		<div class="rmbt-header-wireframes-3__col rmbt-header-wireframes-3-top-col-right">
+			<div class="block-details"> languages, currencies</div>
+		</div>
+		</div>
+		<div class="rmbt-header-wireframes-3__row rmbt-header-wireframes-3-middle-row">
 
-         <div class="rmbt-header-wireframes-3__col rmbt-header-wireframes-3-middle-col-left">
-            <div class="block-details">logo</div>
-         </div>
-         <div class="rmbt-header-wireframes-3__col rmbt-header-wireframes-3-middle-col-center">
-            <div class="block-details">search field</div>
-         </div>
-         <div class="rmbt-header-wireframes-3__col rmbt-header-wireframes-3-middle-col-right">
-            <div class="block-details">Recently Viewed Products, liked, login, cart, total sum</div>
-         </div>
-      </div>
+		<div class="rmbt-header-wireframes-3__col rmbt-header-wireframes-3-middle-col-left">
+			<div class="block-details">logo</div>
+		</div>
+		<div class="rmbt-header-wireframes-3__col rmbt-header-wireframes-3-middle-col-center">
+			<div class="block-details">search field</div>
+		</div>
+		<div class="rmbt-header-wireframes-3__col rmbt-header-wireframes-3-middle-col-right">
+			<div class="block-details">Recently Viewed Products, liked, login, cart, total sum</div>
+		</div>
+		</div>
 
-      <div class="rmbt-header-wireframes-3__row rmbt-header-wireframes-3-bottom-row">
+		<div class="rmbt-header-wireframes-3__row rmbt-header-wireframes-3-bottom-row">
 
-         <div class="rmbt-header-wireframes-3__col rmbt-header-wireframes-3-bottom-col">
-            <div class="block-details">categories menu </div>
-         </div>
+		<div class="rmbt-header-wireframes-3__col rmbt-header-wireframes-3-bottom-col">
+			<div class="block-details">categories menu </div>
+		</div>
 
-      </div>
-   </section>
+		</div>
+	</section>
 </div>

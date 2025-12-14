fce56d297e9857b7d4127186271147c88f6caaea
Rombt <rombtnet@gmail.com>
2025-12-14 12:23:19 +0200

    header-0.php

--- app/src/pages/_templates/headers/header-0/header-0.php
+++ app/src/pages/_templates/headers/header-0/header-0.php
@@ -1,13 +1,19 @@
+<?php
+/**
+ * Header 0 template.
+ *
+ * @package Premium_Theme_1
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
 <section class="rmbt-full-width rmbt-header-wireframes-0-full-width">
-
 	<div class="rmbt-container rmbt-header-wireframes-0">
-
 		<div class="rmbt-header-wireframes-0__row rmbt-header-wireframes-0-row">
-
 			<div class="rmbt-header-wireframes-0__col rmbt-header-wireframes-0-col-logo">
 				<div class="block-details">logo</div>
 			</div>
-
 			<div class="rmbt-header-wireframes-0__col rmbt-header-wireframes-0-col-nav">
 				<div class="block-details block-details-main-header-menu">
 					main header menu
@@ -18,13 +24,10 @@
 				</div>
 				<div class="block-details block-details-search">search field</div>
 			</div>
-
 			<div class="rmbt-header-wireframes-0__col rmbt-header-wireframes-0-col-search">
 				<div class="block-details  block-details-search"> search icon </div>
 				<div class="block-details  block-details-burger">burger-1</div>
 			</div>
-
-
 			<div class="rmbt-header-wireframes-0__col rmbt-header-wireframes-0-col-burger">
 				<div class="block-details">
 					burger-2

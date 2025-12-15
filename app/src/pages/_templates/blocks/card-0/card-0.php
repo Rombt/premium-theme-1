243dea287624fb5912df85c8391d6366ee077961
Rombt <rombtnet@gmail.com>
2025-12-15 10:38:20 +0200

    card-0.php

--- app/src/pages/_templates/blocks/card-0/card-0.php
+++ app/src/pages/_templates/blocks/card-0/card-0.php
@@ -1,21 +1,24 @@
+<?php
+/**
+ * Card Redux Template.
+ *
+ * @package premium-theme-1
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
 <article class="rmbt-card-0">
 	<h3 class='rmbt-card-0__head'>
 		title
-		<?php // echo rmbt_get_redux_field( 'rmbt_card-0_title' ) ?>
-		<?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt_card-0_title')]); ?>
 	</h3>
 	<div class="wrap-img rmbt-card-0__img">
 		img
-		<!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/img/" alt=""> -->
 	</div>
-
 	<div class='rmbt-card-0__body'>
 		text
-		<?php // echo rmbt_get_redux_field( 'rmbt_card-0_text' ) ?>
 	</div>
 	<div class='rmbt-card-0__footer'>
 		<?php get_template_part( 'pages\_templates\components\button_0\button_0', null ); ?>
 	</div>
-
-
 </article>

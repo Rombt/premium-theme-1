0fb369155d68592fc7a8e9151d06933fbd77a368
Rombt <rombtnet@gmail.com>
2025-12-15 14:41:55 +0200

    how-we-work-redux.php

--- app/src/pages/parts/how_we_work/how-we-work-redux.php
+++ app/src/pages/parts/how_we_work/how-we-work-redux.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * How We Work Redux Template.
+ *
+ * @package premium-theme-1
+ */
 
 defined( 'ABSPATH' ) || exit;
 
@@ -11,9 +16,7 @@ Redux::set_section(
 		'desc'             => esc_html__( 'Settings "How We Work Card" block', 'premium-theme-1' ),
 		'customizer_width' => '450',
 		'subsections'      => true,
-		// 'icon'             => 'el el-home',
 		'fields'           => array(
-
 			array(
 				'id'      => 'rmbt-how-we-work-card_section-title',
 				'type'    => 'text',
@@ -25,8 +28,6 @@ Redux::set_section(
 				'type'  => 'text',
 				'title' => __( 'Subtitle of cards block', 'premium-theme-1' ),
 			),
-
-
 			/*------------------ rmbt-how-we-work-card-1 accordion ------------------*/
 			array(
 				'id'       => 'rmbt-how-we-work-card-1-start',
@@ -35,26 +36,21 @@ Redux::set_section(
 				'subtitle' => __( 'Add your content to first card', 'premium-theme-1' ),
 				'position' => 'start',
 			),
-
-
 			array(
 				'id'    => 'how-we-work-card-1_title',
 				'type'  => 'text',
 				'title' => __( 'Title of first card', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'how-we-work-card-1_subtitle',
 				'type'  => 'text',
 				'title' => __( 'Subtitle of first card', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'how-we-work-card-1_text',
 				'type'  => 'textarea',
 				'title' => __( 'Text of first card', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'           => 'rmbt-how-we-work-card-1-img',
 				'type'         => 'media',
@@ -64,21 +60,17 @@ Redux::set_section(
 				'preview_size' => 'full',
 				'remove'       => true,
 			),
-
 			array(
 				'id'    => 'rmbt-how-we-work-card-1-img-alt',
 				'type'  => 'text',
 				'title' => __( 'Image description of first card', 'premium-theme-1' ),
 			),
-
-
 			array(
 				'id'       => 'rmbt-how-we-work-card-1-end',
 				'type'     => 'accordion',
 				'position' => 'end',
 			),
 			/*------------------ /rmbt-how-we-work-card-1 accordion ------------------*/
-
 
 			/*------------------ rmbt-how-we-work-card-2 accordion ------------------*/
 			array(
@@ -88,26 +80,21 @@ Redux::set_section(
 				'subtitle' => 'Add your content to second card',
 				'position' => 'start',
 			),
-
-
 			array(
 				'id'    => 'how-we-work-card-2_title',
 				'type'  => 'text',
 				'title' => __( 'Title of second card', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'how-we-work-card-2_subtitle',
 				'type'  => 'text',
 				'title' => __( 'Subtitle of second card', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'how-we-work-card-2_text',
 				'type'  => 'textarea',
 				'title' => __( 'Text of second card', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'           => 'rmbt-how-we-work-card-2-img',
 				'type'         => 'media',
@@ -117,21 +104,17 @@ Redux::set_section(
 				'preview_size' => 'full',
 				'remove'       => true,
 			),
-
 			array(
 				'id'    => 'rmbt-how-we-work-card-2-img-alt',
 				'type'  => 'text',
 				'title' => __( 'Image description of second card', 'premium-theme-1' ),
 			),
-
-
 			array(
 				'id'       => 'rmbt-how-we-work-card-2-end',
 				'type'     => 'accordion',
 				'position' => 'end',
 			),
 			/*------------------ /rmbt-how-we-work-card-2 accordion ------------------*/
-
 
 			/*------------------ rmbt-how-we-work-card-3 accordion ------------------*/
 			array(
@@ -141,26 +124,21 @@ Redux::set_section(
 				'subtitle' => 'Add your content to third card',
 				'position' => 'start',
 			),
-
-
 			array(
 				'id'    => 'how-we-work-card-3_title',
 				'type'  => 'text',
 				'title' => __( 'Title of third card', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'how-we-work-card-3_subtitle',
 				'type'  => 'text',
 				'title' => __( 'Subtitle of third card', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'how-we-work-card-3_text',
 				'type'  => 'textarea',
 				'title' => __( 'Text of third card', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'           => 'rmbt-how-we-work-card-3-img',
 				'type'         => 'media',
@@ -170,14 +148,11 @@ Redux::set_section(
 				'preview_size' => 'full',
 				'remove'       => true,
 			),
-
 			array(
 				'id'    => 'rmbt-how-we-work-card-3-img-alt',
 				'type'  => 'text',
 				'title' => __( 'Subtitle of third card', 'premium-theme-1' ),
 			),
-
-
 			array(
 				'id'       => 'rmbt-how-we-work-card-3-end',
 				'type'     => 'accordion',
@@ -193,26 +168,21 @@ Redux::set_section(
 				'subtitle' => 'Add your content to fourth card',
 				'position' => 'start',
 			),
-
-
 			array(
 				'id'    => 'how-we-work-card-4_title',
 				'type'  => 'text',
 				'title' => __( 'Title of fourth card', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'how-we-work-card-4_subtitle',
 				'type'  => 'text',
 				'title' => __( 'Subtitle of fourth card', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'how-we-work-card-4_text',
 				'type'  => 'textarea',
 				'title' => __( 'Text of fourth card', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'           => 'rmbt-how-we-work-card-4-img',
 				'type'         => 'media',
@@ -222,23 +192,17 @@ Redux::set_section(
 				'preview_size' => 'full',
 				'remove'       => true,
 			),
-
 			array(
 				'id'    => 'rmbt-how-we-work-card-4-img-alt',
 				'type'  => 'text',
 				'title' => __( 'Subtitle of fourth card', 'premium-theme-1' ),
 			),
-
-
 			array(
 				'id'       => 'rmbt-how-we-work-card-4-end',
 				'type'     => 'accordion',
 				'position' => 'end',
 			),
 			/*------------------ /rmbt-how-we-work-card-4 accordion ------------------*/
-
-
-
 		),
 	)
 );

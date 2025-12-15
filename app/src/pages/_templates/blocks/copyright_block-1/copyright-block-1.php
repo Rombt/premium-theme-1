5e0d2eba3c317cc997b949dffd987a9d9a63e0f0
Rombt <rombtnet@gmail.com>
2025-12-15 10:26:38 +0200

    copyright-block-1.php

---
+++ app/src/pages/_templates/blocks/copyright_block-1/copyright-block-1.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright Block Redux Template.
+ *
+ * @package premium-theme-1
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
+<div class="rmbt-copyright-block-1">
+	<div class='rmbt-copyright-block-1__body'>
+		<?php
+		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo get_icon_svg( 'copyright_1' );
+		?>
+		2024 Rombt Net Studio
+		<br>
+		All rights reserved
+	</div>
+</div>

c7a139ead1d4101dc5d7f95eece2d957d462b9a0
Rombt <rombtnet@gmail.com>
2025-12-15 15:45:50 +0200

    content-page.php

--- app/src/pages/parts/content/content-page.php
+++ app/src/pages/parts/content/content-page.php
@@ -1,23 +1,26 @@
+<?php
+/**
+ * Content Page Template.
+ *
+ * @package premium-theme-1
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
+
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'page' ); ?>>
-
-
-
 	<header class="entry-header">
 		<h1 class="entry-title title-section"><?php the_title(); ?></h1>
 	</header>
-
 	<div class="entry-content">
-
 	<?php if ( has_post_thumbnail() ) : ?>
 		<div class="page__thumb">
 			<?php the_post_thumbnail( 'full' ); ?>
 		</div>
 	<?php endif; ?>
-
 		<?php the_content(); ?>
-
 		<?php
-		// пагинация, если контент разбит на страницы
 		wp_link_pages(
 			array(
 				'before' => '<div class="page-links">',
@@ -26,5 +29,4 @@
 		);
 		?>
 	</div>
-
 </article>

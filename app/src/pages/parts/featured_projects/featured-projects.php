4c9dd64e469d6c633ce9939722baee8f6930dbc5
Rombt <rombtnet@gmail.com>
2025-12-15 15:40:36 +0200

    featured-projects.php

--- app/src/pages/parts/featured_projects/featured-projects.php
+++ app/src/pages/parts/featured_projects/featured-projects.php
@@ -1,54 +1,54 @@
 <?php
+/**
+ * Featured Projects Template.
+ *
+ * @package premium-theme-1
+ */
+
+defined( 'ABSPATH' ) || exit;
+
 $args = array(
-	'post_type'      => 'project', // Ваш кастомный тип записи
-	'posts_per_page' => 5,                // Все записи
+	'post_type'      => 'project',
+	'posts_per_page' => 5,
 );
 
-$posts = get_posts( $args );
+$rmbt_posts = get_posts( $args );
 ?>
 
-<?php if ( ! empty( $posts ) ) : ?>
-
+<?php if ( ! empty( $rmbt_posts ) ) : ?>
 	<div class="wrapper-section featured-projects-wrapper-section">
 		<div class="rmbt-full-width rmbt-featured-projects-full-width">
-			<!-- by container width -->
-			<!-- <section class="rmbt-container rmbt-featured-projects"> -->
-			<!-- by full width -->
 			<section class="rmbt-featured-projects">
-				<?php if ( rmbt_get_redux_field( 'rmbt-featured-projects_section-title', 1 ) != '' ) { ?>
+				<?php if ( '' !== rmbt_get_redux_field( 'rmbt-featured-projects_section-title', 1 ) ) { ?>
 				<h2 class="title-section">
-					<?php echo rmbt_get_redux_field( 'rmbt-featured-projects_section-title', 1 ); ?>
+					<?php echo esc_html( rmbt_get_redux_field( 'rmbt-featured-projects_section-title', 1 ) ); ?>
 				</h2>
 				<?php } ?>
 				<p class="subtitle-section">
-					<?php echo rmbt_get_redux_field( 'rmbt-featured-projects_section-subtitle', 1 ); ?>
+					<?php echo esc_html( rmbt_get_redux_field( 'rmbt-featured-projects_section-subtitle', 1 ) ); ?>
 				</p>
 				<div class="rmbt-featured-projects__row">
 					<div class="rmbt-featured-projects-slide-swiper swiper">
 						<div class="swiper-wrapper">
-
 							<?php
-							foreach ( $posts as $post ) :
-								setup_postdata( $post );
+							foreach ( $rmbt_posts as $rmbt_post ) :
+								setup_postdata( $rmbt_post );
 								?>
-
 								<div class="swiper-slide rmbt-featured-projects-slide  shadow-box">
-									<a href="<?php echo get_permalink(); ?>">
+									<a href="<?php echo esc_url( get_permalink() ); ?>">
 										<div class="wrap-img rmbt-featured-projects-slide__img">
-
 											<?php
 											if ( has_post_thumbnail() ) {
 												the_post_thumbnail( 'medium' );
 											} else {
+												// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 												echo rmbt_redux_img( 'rmbt-coming_soon_img', 'rmbt-coming_soon_img-alt' );
 											}
 											?>
-
 										</div>
 									</a>
 									<h2 class="title-block"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-									<div class='subtitle-block'><?php echo wp_trim_words( get_the_excerpt(), 10, '  [...]' ); ?></div>
-
+									<div class='subtitle-block'><?php echo esc_url( wp_trim_words( get_the_excerpt(), 10, '  [...]' ) ); ?></div>
 									<?php
 									get_template_part(
 										'pages/components/button_read_more/button_read_more',
@@ -62,20 +62,21 @@ $posts = get_posts( $args );
 									);
 									?>
 								</div>
-
 								<?php
 							endforeach;
-							wp_reset_postdata();
+								wp_reset_postdata();
 							?>
-
 						</div>
-						<!-- <div class="rmbt--swiper__pagination"></div> -->
 					</div>
 					<div class="rmbt-featured-projects-slide-swiper__button-next">
-						<?php echo get_icon_svg( 'chevron_1', true ); ?>
+						<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+						echo get_icon_svg( 'chevron_1', true );
+						?>
 					</div>
 					<div class="rmbt-featured-projects-slide-swiper__button-prev">
-						<?php echo get_icon_svg( 'chevron_1', true ); ?>
+						<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+						echo get_icon_svg( 'chevron_1', true );
+						?>
 					</div>
 				</div>
 			</section>

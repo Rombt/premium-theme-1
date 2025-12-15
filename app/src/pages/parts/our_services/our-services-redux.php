288d40c7fc3475e81d41ae7a50b3fcf890077e1c
Rombt <rombtnet@gmail.com>
2025-12-15 14:16:51 +0200

    our-services-redux.php

--- app/src/pages/parts/our_services/our-services-redux.php
+++ app/src/pages/parts/our_services/our-services-redux.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Our Services Redux Template.
+ *
+ * @package premium-theme-1
+ */
 
 defined( 'ABSPATH' ) || exit;
 
@@ -11,9 +16,7 @@ Redux::set_section(
 		'desc'             => esc_html__( 'Settings header site', 'premium-theme-1' ),
 		'customizer_width' => '450',
 		'subsections'      => true,
-		// 'icon'             => 'el el-home',
 		'fields'           => array(
-
 			array(
 				'id'      => 'rmbt-our-services-tabs_section-title',
 				'type'    => 'text',
@@ -25,8 +28,6 @@ Redux::set_section(
 				'type'  => 'text',
 				'title' => __( 'Subtitle of tabs block', 'premium-theme-1' ),
 			),
-
-
 			/*------------------ rmbt-our-services-tabs-1 accordion ------------------*/
 			array(
 				'id'       => 'rmbt-our-services-tabs-1-start',
@@ -35,26 +36,21 @@ Redux::set_section(
 				'subtitle' => __( 'Add your content to first tab', 'premium-theme-1' ),
 				'position' => 'start',
 			),
-
-
 			array(
 				'id'    => 'our-services-tabs-1_title',
 				'type'  => 'text',
 				'title' => __( 'Title of first tab', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'our-services-tabs-1_subtitle',
 				'type'  => 'text',
 				'title' => __( 'Subtitle of first tab', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'our-services-tabs-1_text',
 				'type'  => 'textarea',
 				'title' => __( 'Text of first tab', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'           => 'rmbt-our-services-tabs-1-img',
 				'type'         => 'media',
@@ -64,21 +60,17 @@ Redux::set_section(
 				'preview_size' => 'full',
 				'remove'       => true,
 			),
-
 			array(
 				'id'    => 'rmbt-our-services-tabs-1-img-alt',
 				'type'  => 'text',
 				'title' => __( 'Image description of first tab', 'premium-theme-1' ),
 			),
-
-
 			array(
 				'id'       => 'rmbt-our-services-tabs-1-end',
 				'type'     => 'accordion',
 				'position' => 'end',
 			),
 			/*------------------ /rmbt-our-services-tabs-1 accordion ------------------*/
-
 
 			/*------------------ rmbt-our-services-tabs-2 accordion ------------------*/
 			array(
@@ -88,26 +80,21 @@ Redux::set_section(
 				'subtitle' => 'Add your content to second tab',
 				'position' => 'start',
 			),
-
-
 			array(
 				'id'    => 'our-services-tabs-2_title',
 				'type'  => 'text',
 				'title' => __( 'Title of second tab', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'our-services-tabs-2_subtitle',
 				'type'  => 'text',
 				'title' => __( 'Subtitle of second tab', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'our-services-tabs-2_text',
 				'type'  => 'textarea',
 				'title' => __( 'Text of second tab', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'           => 'rmbt-our-services-tabs-2-img',
 				'type'         => 'media',
@@ -117,21 +104,17 @@ Redux::set_section(
 				'preview_size' => 'full',
 				'remove'       => true,
 			),
-
 			array(
 				'id'    => 'rmbt-our-services-tabs-2-img-alt',
 				'type'  => 'text',
 				'title' => __( 'Image description of second tab', 'premium-theme-1' ),
 			),
-
-
 			array(
 				'id'       => 'rmbt-our-services-tabs-2-end',
 				'type'     => 'accordion',
 				'position' => 'end',
 			),
 			/*------------------ /rmbt-our-services-tabs-2 accordion ------------------*/
-
 
 			/*------------------ rmbt-our-services-tabs-3 accordion ------------------*/
 			array(
@@ -141,26 +124,21 @@ Redux::set_section(
 				'subtitle' => 'Add your content to third tab',
 				'position' => 'start',
 			),
-
-
 			array(
 				'id'    => 'our-services-tabs-3_title',
 				'type'  => 'text',
 				'title' => __( 'Title of third tab', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'our-services-tabs-3_subtitle',
 				'type'  => 'text',
 				'title' => __( 'Subtitle of third tab', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'our-services-tabs-3_text',
 				'type'  => 'textarea',
 				'title' => __( 'Text of third tab', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'           => 'rmbt-our-services-tabs-3-img',
 				'type'         => 'media',
@@ -170,23 +148,17 @@ Redux::set_section(
 				'preview_size' => 'full',
 				'remove'       => true,
 			),
-
 			array(
 				'id'    => 'rmbt-our-services-tabs-3-img-alt',
 				'type'  => 'text',
 				'title' => __( 'Subtitle of third tab', 'premium-theme-1' ),
 			),
-
-
 			array(
 				'id'       => 'rmbt-our-services-tabs-3-end',
 				'type'     => 'accordion',
 				'position' => 'end',
 			),
 			/*------------------ /rmbt-our-services-tabs-3 accordion ------------------*/
-
-
-
 		),
 	)
 );

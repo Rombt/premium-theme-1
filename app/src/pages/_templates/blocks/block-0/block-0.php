f68484c0680ddce3b01132f44631120e25ca081d
Rombt <rombtnet@gmail.com>
2025-12-15 10:47:02 +0200

    block-0.php

--- app/src/pages/_templates/blocks/block-0/block-0.php
+++ app/src/pages/_templates/blocks/block-0/block-0.php
@@ -1,13 +1,18 @@
+<?php
+/**
+ * Block 0 Template.
+ *
+ * @package premium-theme-1
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
 <article class="rmbt-block-0">
 	<h3 class='rmbt-block-0__head block-details'>
 		title
-		<?php // echo rmbt_get_redux_field( 'rmbt_block-0_title' ) ?>
-		<?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt_block-0_title')]); ?>
 	</h3>
 	<p class='rmbt-block-0__body block-details'>
 		text
-		<?php // echo rmbt_get_redux_field( 'rmbt_block-0_text' ) ?>
 	</p>
-
-
 </article>

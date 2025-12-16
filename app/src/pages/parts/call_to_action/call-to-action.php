b8d22fb30a19aa509af290ac20b86c9625f952d0
Rombt <rombtnet@gmail.com>
2025-12-16 10:43:37 +0200

    call-to-action.php

--- app/src/pages/parts/call_to_action/call-to-action.php
+++ app/src/pages/parts/call_to_action/call-to-action.php
@@ -1,34 +1,41 @@
-<?php global $rmbt_theme_options; ?>
+<?php
+/**
+ * Call to action section.
+ *
+ * @package RMBT_Theme
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+
+global $rmbt_theme_options; ?>
 
 
 <div class="wrapper-section call-to-action-wrapper-section">
 	<div class="rmbt-full-width rmbt-call-to-action-full-width">
 		<div class="rmbt-call-to-action-full-width__bg">
 			<div class="wrap-img">
-				<?php echo rmbt_redux_img( 'rmbt-call-to-action-bg-img', 'rmbt-call-to-action-bg-img-alt' ); ?>
+				<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				echo rmbt_redux_img( 'rmbt-call-to-action-bg-img', 'rmbt-call-to-action-bg-img-alt' );
+				?>
 			</div>
 		</div>
-
-		<section class="rmbt-container rmbt-call-to-action" 
-		<?php
-		if ( isset( $rmbt_theme_options['rmbt-call-to-action-card_section-title'] ) && $rmbt_theme_options['rmbt-call-to-action-card_section-title'] == '' ) :
-			echo 'style="padding-top: 15px; padding-bottom: 15px;"';
-		endif
-		?>
-		>
-
+		<section class="rmbt-container rmbt-call-to-action"
+			<?php
+			if ( isset( $rmbt_theme_options['rmbt-call-to-action-card_section-title'] ) && '' === $rmbt_theme_options['rmbt-call-to-action-card_section-title'] ) :
+				echo 'style="padding-top: 15px; padding-bottom: 15px;"';
+			endif
+			?>
 			<?php if ( $rmbt_theme_options['rmbt-call-to-action-card_section-title'] ) : ?>
-				<h2 class='title-section'> <?php echo rmbt_get_redux_field( 'rmbt-call-to-action-card_section-title', 1 ); ?>
+				<h2 class='title-section'> <?php echo esc_html( rmbt_get_redux_field( 'rmbt-call-to-action-card_section-title', 1 ) ); ?>
 				</h2>
 			<?php endif ?>
-			<?php if ( isset( $rmbt_theme_options['rmbt-call-to-action-card_section-subtitle'] ) && $rmbt_theme_options['rmbt-call-to-action-card_section-subtitle'] !== '' ) : ?>
+			<?php if ( isset( $rmbt_theme_options['rmbt-call-to-action-card_section-subtitle'] ) && '' !== $rmbt_theme_options['rmbt-call-to-action-card_section-subtitle'] ) : ?>
 				<p class='subtitle-section'>
-					<?php echo rmbt_get_redux_field( 'rmbt-call-to-action-card_section-subtitle', 1 ); ?>
+					<?php echo esc_html( rmbt_get_redux_field( 'rmbt-call-to-action-card_section-subtitle', 1 ) ); ?>
 				</p>
 			<?php endif ?>
-
 			<div class="rmbt-call-to-action__row">
-
 				<form id="rmbt-call-to-action-form" action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>"
 					method="post" class="rmbt-call-to-action__left-col">
 					<?php wp_nonce_field( 'rmbt_call_to_action_form', 'rmbt_call_to_action_form_nonce' ); ?>
@@ -44,7 +51,6 @@
 					<div class="form-field-group rmbt-call-to-action__email">
 						<label for="email" class="subtitle-block">Email</label>
 						<input type="email" id="email" name="email" class='shadow-box'>
-
 					</div>
 					<div class="form-field-group rmbt-call-to-action__message">
 						<label for="message" class="subtitle-block">Message</label>
@@ -52,21 +58,19 @@
 					</div>
 					<button type="submit" class='rmbt-button-cta'>
 						<span>
-							<?php echo rmbt_get_redux_field( 'rmbt-call-to-action-button-cta', 1 ); ?>
+							<?php echo esc_html( rmbt_get_redux_field( 'rmbt-call-to-action-button-cta', 1 ) ); ?>
 						</span>
 					</button>
 				</form>
-
 				<div id="thank-you-modal" class="modal">
 					<div class="modal-content">
 						<p>
-							<?php echo rmbt_get_redux_field( 'rmbt-call-to-action-modal-content', 1 ); ?>
+							<?php echo esc_html( rmbt_get_redux_field( 'rmbt-call-to-action-modal-content', 1 ) ); ?>
 						</p>
 						<button id="close-modal-btn">Close</button>
 					</div>
 				</div>
 			</div>
-
 		</section>
 	</div>
 </div>

7d9a1aba194e4400166d1fbd036798385006acf7
Rombt <rombtnet@gmail.com>
2025-12-16 11:12:05 +0200

    copyright-block-0-redux.php

--- app/src/pages/components/copyright_block-0/copyright-block-0-redux.php
+++ app/src/pages/components/copyright_block-0/copyright-block-0-redux.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Copyright block 0 Redux options.
+ *
+ * @package RMBT_Theme
+ */
 
 defined( 'ABSPATH' ) || exit;
 
@@ -11,9 +16,7 @@ Redux::set_section(
 		'desc'             => esc_html__( 'Settings header site', 'premium-theme-1' ),
 		'customizer_width' => '450',
 		'subsections'      => true,
-		// 'icon'             => 'el el-home',
 		'fields'           => array(
-
 			array(
 				'id'           => 'rmbt-bg_section-img',
 				'type'         => 'media',
@@ -31,22 +34,16 @@ Redux::set_section(
 				'title'   => esc_html__( 'Description for background section picture', 'premium-theme-1' ),
 				'default' => esc_html__( 'background section picture', 'premium-theme-1' ),
 			),
-
 			array(
 				'id'    => 'rmbt-simple-section_section-title',
 				'type'  => 'text',
 				'title' => esc_html__( 'Title of Simple-Section', 'premium-theme-1' ),
-				// 'default' => esc_html__( '', 'premium-theme-1' ),
 			),
 			array(
 				'id'    => 'rmbt-simple-section_section-text',
 				'type'  => 'text',
 				'title' => esc_html__( 'Text of Simple-Section', 'premium-theme-1' ),
-				// 'default' => esc_html__( '', 'premium-theme-1' ),
 			),
-
-
-
 			/*------------------ rmbt-simple-section accordion ------------------*/
 			array(
 				'id'       => 'rmbt-simple-section-start',
@@ -55,37 +52,12 @@ Redux::set_section(
 				'subtitle' => 'Add your content to the section \'Title\'',
 				'position' => 'start',
 			),
-
-
-			// array(
-			// 'id' => 'rmbt-simple-section-gallery',
-			// 'type' => 'gallery',
-			// 'title' => esc_html__( 'Add/Edit Gallery on the main screen ', 'premium-theme-1' ),
-			// ),
-
-			// array(
-			// 'id' => 'simple-section_title',
-			// 'type' => 'text',
-			// 'title' => esc_html__( 'Front page title', 'premium-theme-1' ),
-			// 'default' => __( wp_kses( 'Український виробник', 'post' ), 'premium-theme-1' ),
-			// ),
-
-			// array(
-			// 'id' => 'simple-section_subtitle',
-			// 'type' => 'text',
-			// 'title' => esc_html__( 'Front page title', 'premium-theme-1' ),
-			// 'default' => __( wp_kses( 'хлібопекарського і кондитерського обладнання', 'post' ), 'premium-theme-1' ),
-			// ),
-
-
 			array(
 				'id'       => 'rmbt-simple-section-end',
 				'type'     => 'accordion',
 				'position' => 'end',
 			),
 			/*------------------ /rmbt-simple-section accordion ------------------*/
-
-
 		),
 	)
 );

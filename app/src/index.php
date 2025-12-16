975b45b7897d54acf01c930f4fb5d00105d740bd
Rombt <rombtnet@gmail.com>
2025-12-16 11:41:49 +0200

    index.php

--- app/src/index.php
+++ app/src/index.php
@@ -1,51 +1,50 @@
-<?php get_header(); ?>
+<?php
+/**
+ * Main template file.
+ *
+ * @package RMBT_Theme
+ */
 
+defined( 'ABSPATH' ) || exit;
+
+get_header(); ?>
 
 
 <main>
-
 	<div class="rmbt-full-width rmbt-blog-full-width">
-		<section class="rmbt-container rmbt-blog" 
-		<?php
-		if ( isset( $rmbt_theme_options['rmbt-blog_page-title'] ) && $rmbt_theme_options['rmbt-blog_page-title'] == '' ) :
-			echo 'style="padding-top: 15px; padding-bottom: 15px;"';
-		endif
-		?>
+		<section class="rmbt-container rmbt-blog"
+			<?php
+			if ( isset( $rmbt_theme_options['rmbt-blog_page-title'] ) && '' === $rmbt_theme_options['rmbt-blog_page-title'] ) :
+				echo 'style="padding-top: 15px; padding-bottom: 15px;"';
+			endif
+			?>
 		>
-
 			<?php if ( $rmbt_theme_options['rmbt-blog_page-title'] ) : ?>
-				<h2 class='title-section'> <?php echo rmbt_get_redux_field( 'rmbt-blog_page-title', 1 ); ?> </h2>
+				<h2 class='title-section'> <?php echo esc_html( rmbt_get_redux_field( 'rmbt-blog_page-title', 1 ) ); ?> </h2>
 			<?php endif ?>
-			<?php if ( isset( $rmbt_theme_options['rmbt-blog_page-subtitle'] ) && $rmbt_theme_options['rmbt-blog_page-subtitle'] !== '' ) : ?>
+			<?php if ( isset( $rmbt_theme_options['rmbt-blog_page-subtitle'] ) && '' !== $rmbt_theme_options['rmbt-blog_page-subtitle'] ) : ?>
 				<p class='subtitle-section'>
-					<?php echo rmbt_get_redux_field( 'rmbt-blog_page-subtitle', 1 ); ?>
+					<?php echo esc_html( rmbt_get_redux_field( 'rmbt-blog_page-subtitle', 1 ) ); ?>
 				</p>
 			<?php endif ?>
-
 			<div class="rmbt-blog__row">
 				<?php
 				global $rmbt_theme_options;
-				if ( $rmbt_theme_options['rmbt-sidebar-switch'] && $rmbt_theme_options['rmbt-sidebar-radio'] === '1' ) {
+				if ( $rmbt_theme_options['rmbt-sidebar-switch'] && '1' === $rmbt_theme_options['rmbt-sidebar-radio'] ) {
 					get_sidebar( 'left' );
 				}
 				?>
-
 				<div class="rmbt-blog__col rmbt-blog-right-col">
 					<?php get_template_part( 'pages/parts/loop/loop' ); ?>
 				</div>
-
 				<?php
-				if ( $rmbt_theme_options['rmbt-sidebar-switch'] && $rmbt_theme_options['rmbt-sidebar-radio'] === '2' ) {
+				if ( $rmbt_theme_options['rmbt-sidebar-switch'] && '2' === $rmbt_theme_options['rmbt-sidebar-radio'] ) {
 					get_sidebar( 'right' );
 				}
 				?>
-
-
 			</div>
 		</section>
 	</div>
-
-
 </main>
 
 <?php get_footer(); ?>

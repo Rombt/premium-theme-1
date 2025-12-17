1a44833538e2f3e4e5821960d371bc259598aba5
Rombt <rombtnet@gmail.com>
2025-12-17 15:11:27 +0200

    404.php

--- app/src/404.php
+++ app/src/404.php
@@ -1,4 +1,13 @@
-<?php get_header(); ?>
+<?php
+/**
+ * 404 Page Template.
+ *
+ * This template is used when a requested page is not found.
+ *
+ * @package premium-theme-1
+ */
+
+get_header(); ?>
 
 
 <main>
@@ -7,14 +16,16 @@
 		<div class="rmbt-full-width rmbt-404-full-width">
 			<div class="rmbt-404-full-width__bg">
 				<div class="wrap-img">
-					<?php echo rmbt_redux_img( 'rmbt-404-bg_img', 'rmbt-call-to-action-bg-img-alt' ); ?>
+					<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+					echo rmbt_redux_img( 'rmbt-404-bg_img', 'rmbt-call-to-action-bg-img-alt' );
+					?>
 				</div>
 			</div>
 			<section class="rmbt-container rmbt-404">
 				<div class="rmbt-404__row">
 					<div class="rmbt-404__col ">
 						<h1 class='title-section'>4<span>0</span>4 </h1>
-						<p class='subtitle-section'><?php echo rmbt_get_redux_field( 'rmbt-404_text', 1 ); ?></p>
+						<p class='subtitle-section'><?php echo esc_html( rmbt_get_redux_field( 'rmbt-404_text', 1 ) ); ?></p>
 						<?php
 						get_template_part(
 							'pages/components/button_cta/button_cta',

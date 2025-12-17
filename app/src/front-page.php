273c0f60c7c5048a0e8897e93656976ccba7e779
Rombt <rombtnet@gmail.com>
2025-12-17 10:47:56 +0200

    front-page.php

--- app/src/front-page.php
+++ app/src/front-page.php
@@ -1,14 +1,23 @@
-<?php get_header(); ?>
+<?php
+/**
+ * Front Page Template.
+ *
+ * @package rmbt
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+get_header(); ?>
 
 <main>
 	<?php
 
 	get_template_part( 'pages/parts/our_services/our_services' );
 	get_template_part( 'pages/parts/featured_projects/featured_projects' );
-	get_template_part( 'pages/parts/how_we_work/how_we_work' );     // block of cards, redux fields
-	get_template_part( 'pages/parts/call_to_action/call_to_action' );       // block with title, text and button , redux fields
-	get_template_part( 'pages/parts/post_card-latest_blog/post_card-latest_blog' );     // block of cards
-	get_template_part( 'pages/parts/testimonials/testimonials' );       // block of cards
+	get_template_part( 'pages/parts/how_we_work/how_we_work' );
+	get_template_part( 'pages/parts/call_to_action/call_to_action' );
+	get_template_part( 'pages/parts/post_card-latest_blog/post_card-latest_blog' );
+	get_template_part( 'pages/parts/testimonials/testimonials' );
 
 	?>
 </main>
